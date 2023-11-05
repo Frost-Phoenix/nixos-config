f40d01487b6a60e2c49724651e575c89a3c092a9
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-05 17:21:54 +0100

    clean up

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,5 +4,4 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_lqx;
-  # boot.initrd.kernelModules = [ "amdgpu" ];
 }
