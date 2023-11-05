e0e976077d98b8635cd8b951ad69e9b36a007983
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-05 14:48:55 +0100

    update bootlaoder

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,5 +4,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_lqx;
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  # boot.initrd.kernelModules = [ "amdgpu" ];
 }
