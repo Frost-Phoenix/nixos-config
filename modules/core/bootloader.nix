6f8782ca41a912afbab349e2044e0268022eb6c8
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-05 19:19:47 +0100

    add bootloader limt nb of entry

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,5 +2,6 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.kernelPackages = pkgs.linuxPackages_lqx;
 }
