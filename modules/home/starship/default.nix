4c779277a1e2e61ea8515b4d4e4c927308959b6c
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 23:16:35 +0100

    test starship config

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -8,10 +8,24 @@
     enableNushellIntegration = true;
 
     settings = {
-      character = {
-        success_symbol = "[ ](bold #89dceb)[›](bold green)";
-        error_symbol = "[ ](bold #89dceb)[›](bold red)";
-      };
+      format = lib.concatStrings [
+        #"[ ](fg:#1e1e2e bg:#ff00aa)"
+        #"$username"
+        #"[](fg:#ff00aa)"
+        "$directory"
+        "[](fg:#1e1e2e bg:#cba6f7)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:#cba6f7 bg:#89b4fa)"
+        "[](fg:#89b4fa bg:#94e2d5)"
+        "$nix_shell"
+        "[ ](fg:#94e2d5)"
+      ];
+
+      # character = {
+      #   success_symbol = "[ ](bold #89dceb)[›](bold green)";
+      #   error_symbol = "[ ](bold #89dceb)[›](bold red)";
+      # };
 
       # directory.substitutions = {
       #   "Documents" = " ";
