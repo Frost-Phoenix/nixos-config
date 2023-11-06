fd169d653477f5025ebd2f020b8f144d0cdbc601
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 16:48:13 +0100

    fix bat config

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -7,9 +7,10 @@
       theme = "Catppuccin-mocha";
     };
     themes = {
+      src = inputs.catppuccin-bat;
       # https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme
       Catppuccin-mocha = {
-        file = builtins.readFile "${inputs.catppuccin-bat}/Catppuccin-mocha.tmTheme";
+        file = "Catppuccin-mocha.tmTheme";
       };
     };
   };
