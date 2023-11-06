d8061c7ec1cc2269cb8ed0e8e459bdf117839371
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 21:43:46 +0100

    fix

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -11,7 +11,7 @@
     };
 
     settings = {
-      confirm_os_window_close = false;
+      confirm_os_window_close = 0;
       background_opacity = "0.55";  
       window_padding_width = 5;
       scrollback_lines = 10000;
