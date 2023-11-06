27caa10b74f38431005fb74178a6033b6359b823
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 23:17:45 +0100

    test starship config

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: 
+{ lib, inputs, ... }: 
 {
   programs.starship = {
     enable = true;
@@ -9,17 +9,10 @@
 
     settings = {
       format = lib.concatStrings [
-        #"[ ](fg:#1e1e2e bg:#ff00aa)"
-        #"$username"
-        #"[](fg:#ff00aa)"
         "$directory"
-        "[](fg:#1e1e2e bg:#cba6f7)"
         "$git_branch"
         "$git_status"
-        "[](fg:#cba6f7 bg:#89b4fa)"
-        "[](fg:#89b4fa bg:#94e2d5)"
         "$nix_shell"
-        "[ ](fg:#94e2d5)"
       ];
 
       # character = {
