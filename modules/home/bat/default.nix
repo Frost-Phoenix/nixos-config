87543ab4077c0fbbcd8087b0e377d94f6dfa532e
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 15:53:03 +0100

    change bat config

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -1,10 +1,19 @@
 { config, pkgs, ... }:
+let catppuccin-bat = {
+  url = "github:catppuccin/bat";
+  flake = false;
+};
+in 
 {
  	programs.bat = {
-		enable = true;
-		config = {
-			theme = "Dracula";
-			italic-text = "always";
-		};
-	};
+    enable = true;
+    config = {
+      pager = "less -FR";
+      theme = "Catppuccin-mocha";
+    };
+    themes = {
+      # https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme
+      Catppuccin-mocha = builtins.readFile "${catppuccin-bat}/Catppuccin-mocha.tmTheme";
+    };
+  };
 }
