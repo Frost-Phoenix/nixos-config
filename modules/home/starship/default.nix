e5616b49012683842362d41049c5e7ef92768d31
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 22:48:30 +0100

    test starship config

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -1,4 +1,5 @@
-{ inputs, ...}: {
+{ inputs, ... }: 
+{
   programs.starship = {
     enable = true;
 
@@ -7,11 +8,23 @@
     enableNushellIntegration = true;
 
     settings = {
-      character = {
-        success_symbol = "[›](bold green)";
-        error_symbol = "[›](bold red)";
+      # character = {
+      #   success_symbol = "[›](bold green)";
+      #   error_symbol = "[›](bold red)";
+      # };
+      # character = {
+      #   success_symbol = "[󰊠 ](bold #cba6f7)[󰊠 ](bold #f2cdcd)[󰊠 ](bold #b4befe)[ ](bold #a6e3a1)";
+      #   error_symbol = "[󰊠 ](bold #cba6f7)[󰊠 ](bold #f2cdcd)[󰊠 ](bold #b4befe)[ ](bold #f38ba8)";
+      # };
+
+      directory.substitutions = {
+        "Documents" = " ";
+        "Downloads" = " ";
+        "Music" = " ";
+        "Pictures" = " ";
+        "~" = "󰋞 ";
       };
-      
+
       palette = "catppuccin_mocha";
     } // builtins.fromTOML (builtins.readFile "${inputs.catppuccin-starship}/palettes/mocha.toml");
   };
