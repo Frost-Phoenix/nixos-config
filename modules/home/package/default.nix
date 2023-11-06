cf1ff36ed59f16c0da40629e4b263c96ff65dd50
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-06 17:54:06 +0100

    add aseprite

--- modules/home/package/default.nix
+++ modules/home/package/default.nix
@@ -1,38 +1,36 @@
 { inputs, pkgs, ... }: 
 {
   home.packages = (with pkgs; [
-      bat
-      btop
-      discord
-      eza
-      fzf
-      lazygit
-      nix-prefetch-github
-      python39
-      ripgrep
-      rnix-lsp
-      rustup
-      vscode
-      xfce.thunar
+    aseprite
+    bat
+    btop
+    discord
+    eza
+    fzf
+    lazygit
+    nix-prefetch-github
+    python39
+    ripgrep
+    rnix-lsp
+    rustup
+    vscode
+    xfce.thunar
 
-      bleachbit
-      bore-cli
-      gparted
-      ffmpeg
-      imv
-      libnotify
-      mpv
-      openssl
-      pamixer
-      pavucontrol
-      playerctl
-      qalculate-gtk
-      unzip
-      wget
-      xdg-utils
-      inputs.alejandra.defaultPackage.${system}
-    ]
-    ++ [
-
-    ]);
+    bleachbit
+    bore-cli
+    gparted
+    ffmpeg
+    imv
+    libnotify
+    mpv
+    openssl
+    pamixer
+    pavucontrol
+    playerctl
+    qalculate-gtk
+    unzip
+    wget
+    xdg-utils
+    inputs.alejandra.defaultPackage.${system}
+  ]
 }
