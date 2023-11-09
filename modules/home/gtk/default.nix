8d9f9a194609a9cd996fc446a00cb7cc155b1f24
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-09 22:48:17 +0100

    change gtk theme to catpuccin

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -1,38 +1,42 @@
 { pkgs, ... }:
+let
+  matrix-gtk = pkgs.fetchFromGitHub {
+    owner = "samiulbasirfahim";
+    repo = "Matrix-gtk";
+    rev = "9ff060e016e5cd4830aafb6299142c3d8922944e";
+    sha256 = "K7tEtB0WGKyjND+pZKUd4pktBlCV3v27pvqaxMILpFU=";
+  };
+in
 {
   fonts.fontconfig.enable = true;
   home.packages = [
-    pkgs.nerdfonts
-    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     pkgs.twemoji-color-font
+    pkgs.noto-fonts
+    pkgs.noto-fonts-cjk
     pkgs.noto-fonts-emoji
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
+  xdg.dataFile."themes/matrix-gtk".source = matrix-gtk;
   gtk = {
     enable = true;
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 12;
+      size = 10;
     };
     iconTheme = {
       package = pkgs.papirus-icon-theme;
       name = "Papirus-Dark";
     };
     theme = {
-      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "pink" ];
-        size = "compact";
-        tweaks = [ "rimless" "black" ];
-        variant = "macchiato";
-      };
+      name = "matrix-gtk";
     };
     cursorTheme = {
-      name = "Catppuccin-Mocha-Dark";
+      name = "Catppuccin-Frappe-Dark";
     };
   };
   home.pointerCursor = {
     package = pkgs.catppuccin-cursors;
-    name = "Catppuccin-Mocha-Red";
+    name = "Catppuccin-Frappe-Red";
     size = 15;
   };
 }
