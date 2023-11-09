79358e96b04ce1cc3fda7ec6e2ab21a9720a637a
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-09 22:03:43 +0100

    change firefox config

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -7,7 +7,6 @@
       settings = {};
       isDefault = true;
       extraConfig = builtins.readFile ./user.js;
-      userChrome = builtins.readFile ./userChrome.css;
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         return-youtube-dislikes
