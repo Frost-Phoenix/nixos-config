199578f93a9ea828bcb0d395a19a818e91cbfe95
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-10 21:18:40 +0100

    change hyprland congif

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -15,9 +15,9 @@ in
       exec-once = systemctl --user import-environment &
       exec-once = hash dbus-update-activation-environment 2>/dev/null &
       exec-once = dbus-update-activation-environment --systemd &
-      exec-once = swaybg -m fill -i ~/Pictures/wallpapers/wallpaper.png
-      exec-once = sleep 0.5 && swaylock
-      exec-once = hyprctl setcursor Catppuccin-Frappe-Dark 15
+      exec-once = swaybg -m fill -i ~/Pictures/wallpapers/wallpaper.png &
+      exec-once = sleep 1 && swaylock
+      exec-once = hyprctl setcursor Catppuccin-Frappe-Dark 15 &
       exec-once = waybar &
       exec-once = mako &
 
