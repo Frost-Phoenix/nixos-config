35d91e9d6995637e64529dca2facedbcca1aaa8b
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-10 19:42:51 +0100

    change hyprland congif

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -58,8 +58,8 @@ in
       dwindle {
           no_gaps_when_only = false
           # force_split = 0
-          special_scale_factor = 0.8
-          split_width_multiplier = 1.0
+          # special_scale_factor = 0.8
+          # split_width_multiplier = 1.0
           use_active_for_splits = true
           pseudotile = yes
           preserve_split = yes
