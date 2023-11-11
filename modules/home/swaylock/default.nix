6d9137ccf05cec0f944b1979438edc96a987fcef
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-11 23:31:44 +0100

    change swaylock theme

--- modules/home/swaylock/default.nix
+++ modules/home/swaylock/default.nix
@@ -5,17 +5,32 @@
     package = pkgs.swaylock-effects;
     settings = {
       clock = true;
+      datestr = "";
       screenshots = true;
+      
       indicator = true;
       indicator-radius = 100;
       indicator-thickness = 7;
-      datestr = "";
+      
+      
       effect-blur = "7x5";
       effect-vignette = "0.5:0.5";
+      
       color="1e1e2e";
       bs-hl-color="f5e0dc";
       caps-lock-bs-hl-color="f5e0dc";
       caps-lock-key-hl-color="a6e3a1";
+      ring-color="b4befe";
+      ring-clear-color="f5e0dc";
+      ring-caps-lock-color="fab387";
+      ring-ver-color="89b4fa";
+      ring-wrong-color="eba0ac";
+      text-color="cdd6f4";
+      text-clear-color="f5e0dc";
+      text-caps-lock-color="fab387";
+      text-ver-color="89b4fa";
+      text-wrong-color="eba0ac";
+
       inside-color="00000000";
       inside-clear-color="00000000";
       inside-caps-lock-color="00000000";
@@ -30,17 +45,7 @@
       line-caps-lock-color="00000000";
       line-ver-color="00000000";
       line-wrong-color="00000000";
-      ring-color="b4befe";
-      ring-clear-color="f5e0dc";
-      ring-caps-lock-color="fab387";
-      ring-ver-color="89b4fa";
-      ring-wrong-color="eba0ac";
       separator-color="00000000";
-      text-color="cdd6f4";
-      text-clear-color="f5e0dc";
-      text-caps-lock-color="fab387";
-      text-ver-color="89b4fa";
-      text-wrong-color="eba0ac";
     };
   };
 
