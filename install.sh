33398dbad1b1d22661dbde4188016aa0cd868952
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-11 18:12:11 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 cp /etc/nixos/hardware-configuration.nix hosts/nixos/hardware-configuration.nix
 sleep 1
+mkdir -p ~/Music
+mkdir -p ~/Documents
 mkdir -p ~/Pictures/wallpapers
 sleep 1
 cp ./wallpapers/*  ~/Pictures/wallpapers/
