6faab56dcc22b8462f0f98341952f884c089b162
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-12 21:23:15 +0100

    change portals config

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -4,7 +4,7 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    xdgOpenUsePortal = true;
+    # xdgOpenUsePortal = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
       # pkgs.xdg-desktop-portal-gtk
