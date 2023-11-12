6a3f5c61481b93777fc2dad7f1da201ad0b17466
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-12 14:53:01 +0100

    add extra portals configuration

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -4,6 +4,7 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
+    xdgOpenUsePortal = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
       # pkgs.xdg-desktop-portal-gtk
