a690b5291e784e90445a6af6a028a6338b5862da
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-13 11:28:04 +0100

    change wallpapers path

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@ cp /etc/nixos/hardware-configuration.nix hosts/nixos/hardware-configuration.nix
 sleep 1
 mkdir -p ~/Music
 mkdir -p ~/Documents
-mkdir -p ~/Pictures/wallpapers
+mkdir -p ~/Pictures/wallpapers/others
 sleep 1
-cp ./wallpapers/*  ~/Pictures/wallpapers/
+cp wallpapers/wallpaper.png ~/Pictures/wallpapers
+cp wallpapers/otherWallpaper/* ~/Pictures/wallpapers/others
 sleep 1
 sudo nixos-rebuild switch --flake .#nixos
