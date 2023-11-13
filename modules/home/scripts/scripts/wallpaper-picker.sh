f6dc4934b978220628ff3f2bb77cf7d01e5ffc92
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-14 00:33:21 +0100

    change wallpaper

--- modules/home/scripts/scripts/wallpaper-picker.sh
+++ modules/home/scripts/scripts/wallpaper-picker.sh
@@ -5,6 +5,7 @@ wallpapers_folder=$HOME/Pictures/wallpapers/others
 wallpaper_name="$(ls $wallpapers_folder | wofi -n --show dmenu --sort-order=alphabetical)"
 if [[ -f $wallpapers_folder/$wallpaper_name ]]; then
     find ~/Pictures/wallpapers -maxdepth 1 -type f -delete
+    cp $wallpapers_folder/$wallpaper_name $wallpaper_path/$wallpaper_name
     wall-change $wallpaper_path/$wallpaper_name
 else
     exit 1
