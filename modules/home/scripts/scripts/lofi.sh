af58e41055f408ef44178f816c77b2ec97ab8b06
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-13 23:34:59 +0100

    change scripts

--- modules/home/scripts/scripts/lofi.sh
+++ modules/home/scripts/scripts/lofi.sh
@@ -3,5 +3,5 @@
 if (ps aux | grep mpv | grep -v grep > /dev/null) then
     pkill mpv
 else
-    mpv --no-video https://www.youtube.com/live/jfKfPfyJRdk?si=OF0HKrYFFj33BzMo
+    runbg mpv --no-video https://www.youtube.com/live/jfKfPfyJRdk?si=OF0HKrYFFj33BzMo
 fi
