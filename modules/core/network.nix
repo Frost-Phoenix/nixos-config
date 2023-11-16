9e595cccda57522b0793e9a0ed9aa6e83c16e541
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-16 17:35:49 +0100

    rool back network config

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,13 +2,13 @@
 {
   networking = {
     hostName = "nixos";
-    # networkmanager.enable = true;
-    wireless.enable = true;
-    wireless.userControlled.enable = true;
+    networkmanager.enable = true;
+    # wireless.enable = true;
+    # wireless.userControlled.enable = true;
     nameservers = [ "1.1.1.1" ];
   };
 
-  environment.systemPackages = with pkgs; [
-    wpa_supplicant
-  ];
+  # environment.systemPackages = with pkgs; [
+    # wpa_supplicant
+  # ];
 }
