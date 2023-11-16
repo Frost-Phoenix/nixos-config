72b6d521ac4027575f1137f01b54f8fa68a511d8
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-16 14:44:50 +0100

    add wireless connection suport

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,7 +2,7 @@
 {
   networking = {
     hostName = "nixos";
-    networkmanager.enable = true;
+    # networkmanager.enable = true;
     wireless.enable = true;
     wireless.userControlled.enable = true;
     nameservers = [ "1.1.1.1" ];
