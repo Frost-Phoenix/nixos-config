2912f736c4a4b4a4e6fd433686db90231a39da3f
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-16 14:33:58 +0100

    add RedistributableFirmware option

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -3,6 +3,7 @@
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
+  hardware.enableRedistributableFirmware = true;
   hardware.opengl.extraPackages = with pkgs; [
     
   ];
