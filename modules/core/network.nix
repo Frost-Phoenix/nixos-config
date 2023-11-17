0c8c75d7f178bc7f5493353703064669355a7002
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-17 22:01:32 +0100

    test new wifi tool

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,12 +3,15 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
+    wireless.iwd.enable = true;
+    networkmanager.wifi.backend = "iwd";
     # wireless.enable = true;
     # wireless.userControlled.enable = true;
     nameservers = [ "1.1.1.1" ];
   };
 
-  # environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     # wpa_supplicant
-  # ];
+    iwd
+  ];
 }
