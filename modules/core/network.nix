99b9c865966388eea9f45aea6bb85ed68517600e
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-22 19:40:26 +0100

    wifi is finaly working

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,8 +3,6 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
-    # wireless.iwd.enable = true;
-    # networkmanager.wifi.backend = "iwd";
     nameservers = [ "1.1.1.1" ];
   };
 
--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -15,6 +15,7 @@ in
       exec-once = systemctl --user import-environment &
       exec-once = hash dbus-update-activation-environment 2>/dev/null &
       exec-once = dbus-update-activation-environment --systemd &
+      exec-once = nm-applet &
       exec-once = swaybg -m fill -i $(find ~/Pictures/wallpapers/ -maxdepth 1 -type f) &
       exec-once = sleep 1 && swaylock
       exec-once = hyprctl setcursor Catppuccin-Frappe-Dark 15 &
