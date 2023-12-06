5374ebd9a2a11d6ef279f1fb42c78fe7e581d483
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-06 18:53:50 +0100

    change vscodium settings

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -12,19 +12,24 @@
     userSettings = {
       "update.mode" = "none";
       "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
+      "window.titleBarStyle" = "custom"; # needed otherwise vscode crashes, see https://github.com/NixOS/nixpkgs/issues/246509
 
       "window.menuBarVisibility" = "toggle";
       "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'SymbolsNerdFont', 'monospace', monospace";
       "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font', 'SymbolsNerdFont'";
+      "editor.fontSize" = "16";
       "workbench.colorTheme" = "Catppuccin Mocha";
       "workbench.iconTheme" = "catppuccin-mocha";
       "vsicons.dontShowNewVersionMessage" = true;
       "editor.fontLigatures" = true;
       "editor.minimap.enabled" = false;
 
-      "window.titleBarStyle" = "custom"; # needed otherwise vscode crashes, see https://github.com/NixOS/nixpkgs/issues/246509
-    
+
+      "editor.mouseWheelZoom" = true;
+
+      # keyboard shortcuts    
       "editor.action.commentLine" = "ctrl+d";
+      "workbench.action.files.saveFiles" = "ctrl+s";
     };
   };
 }
