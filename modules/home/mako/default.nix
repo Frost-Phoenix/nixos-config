6d391e313878e6a591c5b00f4a0e2b6d102383dd
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-06 19:26:19 +0100

    change mako config

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -4,18 +4,18 @@
     mako = {
       enable = true;
       font = "JetBrainsMono Nerd Font 11";
-      padding = "20";
+      padding = "15";
       defaultTimeout = 5000;
       borderSize = 2;
-      borderRadius = 0;
-      backgroundColor = "#1F1F1F";
-      borderColor = "#595959";
+      borderRadius = 5;
+      backgroundColor = "#1e1e2e";
+      borderColor = "#89b4fa";
       progressColor = "over #313244";
       textColor = "#cdd6f4";
       extraConfig = ''
         text-alignment=center
         [urgency=high]
-        border-color=#B45C65
+        border-color=#fab387
       '';
     };
   };
