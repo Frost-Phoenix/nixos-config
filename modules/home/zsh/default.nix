8017fe1036751ea65db759eb774bdd31e522ade6
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-06 22:12:38 +0100

    add zsh fzf plugin

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -7,7 +7,7 @@
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git", "fzf" ];
+      plugins = [ "git" "fzf" ];
     };
     shellAliases = {
       # record = "wf-recorder --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor -f $HOME/Videos/$(date +'%Y%m%d%H%M%S_1.mp4')";
