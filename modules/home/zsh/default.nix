9f0dbe4eddc47f4071c418b9ec08ecfe056df774
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-14 14:02:18 +0100

    try fix zsh slow pasting

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -9,6 +9,7 @@
       enable = true;
       plugins = [ "git" "fzf" ];
     };
+    initExtra = "DISABLE_MAGIC_FUNCTIONS=true";
     shellAliases = {
       # record = "wf-recorder --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor -f $HOME/Videos/$(date +'%Y%m%d%H%M%S_1.mp4')";
 
