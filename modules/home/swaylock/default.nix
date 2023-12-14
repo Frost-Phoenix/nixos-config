112ba74a77756e6512ce3547b83899d9faf589cf
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-14 16:02:50 +0100

    disable swayidle

--- modules/home/swaylock/default.nix
+++ modules/home/swaylock/default.nix
@@ -49,35 +49,35 @@
     };
   };
 
-  services.swayidle = {
-    enable = true;
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-      }
-      {
-        event = "lock";
-        command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-      }
-    ];
-    timeouts = [
-      {
-        timeout = 90;
-        command = "swaylock";
-      }
-      {
-        timeout = 300;
-        command = "systemctl suspend";
-      }
-      # {
-      #   timeout = 180;
-      #   command = "systemctl suspend";
-      #   # command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
-      #   # resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
-      # }
-    ];
-  };
+  # services.swayidle = {
+  #   enable = true;
+  #   events = [
+  #     {
+  #       event = "before-sleep";
+  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+  #     }
+  #     {
+  #       event = "lock";
+  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+  #     }
+  #   ];
+  #   timeouts = [
+  #     {
+  #       timeout = 90;
+  #       command = "swaylock";
+  #     }
+  #     {
+  #       timeout = 300;
+  #       command = "systemctl suspend";
+  #     }
+  #     {
+  #       timeout = 180;
+  #       command = "systemctl suspend";
+  #       # command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
+  #       # resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
+  #     }
+  #   ];
+  # };
 
-  systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
+  # systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
 }
