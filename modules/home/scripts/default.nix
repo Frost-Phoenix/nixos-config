bc93476875cdb87d2033dcd64304e22c89edf586
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-14 16:00:18 +0100

    clean up

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -1,17 +1,23 @@
 {pkgs, ...}: let
   wall-change = pkgs.writeShellScriptBin "wall-change" ''swaybg -m fill -i $1'';
   wallpaper-picker = pkgs.writeShellScriptBin "wallpaper-picker" (builtins.readFile ./scripts/wallpaper-picker.sh);
+  
   push = pkgs.writeShellScriptBin "push" (builtins.readFile ./scripts/push.sh);
   commit = pkgs.writeShellScriptBin "commit" (builtins.readFile ./scripts/commit.sh);
+  
   dsize = pkgs.writeShellScriptBin "dsize" (builtins.readFile ./scripts/dsize.sh);
   runbg = pkgs.writeShellScriptBin "runbg" (builtins.readFile ./scripts/runbg.sh);
   music = pkgs.writeShellScriptBin "music" (builtins.readFile ./scripts/music.sh);
+  lofi = pkgs.writeScriptBin "lofi" (builtins.readFile ./scripts/lofi.sh);
+  
   toogle_blur = pkgs.writeScriptBin "toogle_blur" (builtins.readFile ./scripts/toogle_blur.sh);
   toogle_oppacity = pkgs.writeScriptBin "toogle_oppacity" (builtins.readFile ./scripts/toogle_oppacity.sh);
+  
   maxfetch = pkgs.writeScriptBin "maxfetch" (builtins.readFile ./scripts/maxfetch.sh);
-  lofi = pkgs.writeScriptBin "lofi" (builtins.readFile ./scripts/lofi.sh);
+  
   compress = pkgs.writeScriptBin "compress" (builtins.readFile ./scripts/compress.sh);
   extract = pkgs.writeScriptBin "extract" (builtins.readFile ./scripts/extract.sh);
+  
   shutdown-script = pkgs.writeScriptBin "shutdown-script" (builtins.readFile ./scripts/shutdown-script.sh);
 in {
   home.packages = with pkgs; [
@@ -24,13 +30,12 @@ in {
     dsize
     runbg
     music
+    lofi
   
     toogle_blur
     toogle_oppacity
 
     maxfetch
-
-    lofi
 
     compress
     extract
