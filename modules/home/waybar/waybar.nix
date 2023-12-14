3b07ce4f54c7b8f17ab3450d9eeb82e9e01c2adf
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-14 16:04:26 +0100

    clean up

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -2,10 +2,6 @@
 {
   programs.waybar = {
     enable = true;
-    # systemd = {
-    #   enable = false;
-    #   target = "graphical-session.target";
-    # };
   };
   programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
     mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
