f1309de1513474ecbbd44f6013ae7f6bbfe648a0
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-14 15:51:14 +0100

    clean up

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -6,12 +6,5 @@
       pager = "less -FR";
       theme = "Dracula";
     };
-    # themes = {
-    #   src = inputs.catppuccin-bat;
-    #   # https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme
-    #   Catppuccin-mocha = {
-    #     file = "Catppuccin-mocha.tmTheme";
-    #   };
-    # };
   };
 }
