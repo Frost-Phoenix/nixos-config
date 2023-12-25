6f120e2fd1e6c60b143cc7d1b22b86c9493ac915
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-25 20:45:50 +0100

    test hyprland shadows

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -49,6 +49,7 @@ in
         border_size = 2
         col.active_border = rgb(cba6f7) rgb(94e2d5) 45deg
         col.inactive_border = 0x00000000
+        border_part_of_window = false
       }
 
       xwayland {
