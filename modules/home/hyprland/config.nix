f8b1badf1d3318a74648a07bc34b2dc14748103d
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-25 18:02:30 +0100

    test hyprland shadows

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -94,7 +94,13 @@ in
           xray = true
         }
 
-        drop_shadow = 0
+        # drop_shadow = 0
+        drop_shadow = true;
+        shadow_ignore_window = true;
+        shadow_offset = 0 2
+        shadow_range = 20
+        shadow_render_power = 3
+        col.shadow = rgba(00000055)
       }
 
 
