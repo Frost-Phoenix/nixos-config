60076eaea555e49714e649d8cef36d3eefbf5f1a
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-25 14:52:43 +0100

    open some firewall ports

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -4,6 +4,15 @@
     hostName = "nixos";
     networkmanager.enable = true;
     nameservers = [ "1.1.1.1" ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 80 443 59010 59011 ];
+      allowedUDPPorts = [ 59010 59011 ];
+      # allowedUDPPortRanges = [
+        # { from = 4000; to = 4007; }
+        # { from = 8000; to = 8010; }
+      # ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
