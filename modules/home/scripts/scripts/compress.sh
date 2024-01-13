1bb8a4c3daa03611c373797c1f64ff0cbc92238a
Frost-Phoenix <67cyril6767@gmail.com>
2024-01-13 15:37:46 +0100

    simplify script

--- modules/home/scripts/scripts/compress.sh
+++ modules/home/scripts/scripts/compress.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 if (( $# == 1 )) then
-    echo -ne "Archive name: "
-    read name
-    tar -cvzf "$name.tar.gz" $1
+    # echo -ne "Archive name: "
+    # read name
+    # tar -cvzf "$name.tar.gz" $1
+    tar -cvzf "$1.tar.gz" $1
 else
     echo "Wrong number of arguments..."
 fi
