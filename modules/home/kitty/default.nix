8567f63c4daa402a5868b8a6328b0525934fbbd3
Frost-Phoenix <67cyril6767@gmail.com>
2024-01-18 20:36:22 +0100

    rool back

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -12,7 +12,7 @@
 
     settings = {
       confirm_os_window_close = 0;
-      # background_opacity = "0.55";  
+      background_opacity = "0.55";  
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
