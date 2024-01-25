7d90bfb7517ecca346787c01fdef16f245c120b8
Frost-Phoenix <67cyril6767@gmail.com>
2024-01-25 17:00:44 +0100

    change python config

--- modules/home/package/python.nix
+++ modules/home/package/python.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: 
 {
   environment.systemPackages = with pkgs; [
-    (python311Full.withPackages(ps: with ps; [ 
-      requests
-      pygame
-      shapely
-    ]))
+    python3
+    python311Packages.pip
+    # (python311Full.withPackages(ps: with ps; [ 
+    #   requests
+    #   pygame
+    #   shapely
+    # ]))
   ];
 }
