69d8c64451b99ede59400b053677804a73358577
Frost-Phoenix <67cyril6767@gmail.com>
2024-01-25 17:06:17 +0100

    rool back

--- modules/home/package/python.nix
+++ modules/home/package/python.nix
@@ -1,12 +1,10 @@
 {pkgs, ...}: 
 {
   environment.systemPackages = with pkgs; [
-    python3
-    python311Packages.pip
-    # (python311Full.withPackages(ps: with ps; [ 
-    #   requests
-    #   pygame
-    #   shapely
-    # ]))
+    (python311Full.withPackages(ps: with ps; [ 
+      requests
+      pygame
+      shapely
+    ]))
   ];
 }
