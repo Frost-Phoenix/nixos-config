1cb5cb58dc4c28978ee0b3698a1711f92458fb7c
Frost-Phoenix <67cyril6767@gmail.com>
2024-01-29 22:26:57 +0100

    clean up

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -1,8 +1,4 @@
 { ... }:
-let
-  color = (import ../../variables/colors.nix);
-  window_manager = (import ../../variables/window_manager.nix);
-in
 {
   wayland.windowManager.hyprland = {
     extraConfig = "
