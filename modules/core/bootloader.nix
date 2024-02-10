6c7d1f0f603fb27613f7ec3e7031f7ba51eab075
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-10 23:41:56 +0100

    change boot config

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,6 +2,6 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_lqx;
 }
