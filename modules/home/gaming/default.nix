2588595663e8853c5084951bac4907cc885b37d7
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-10 14:57:49 +0100

    remove unused package

--- modules/home/gaming/default.nix
+++ modules/home/gaming/default.nix
@@ -1,25 +1,28 @@
 { pkgs, config, inputs, ... }: 
 {
   home.packages = with pkgs;[
+    ## Utils
     gamemode
     gamescope
     winetricks
-    inputs.nix-gaming.packages.${pkgs.system}.proton-ge
-    inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+    # inputs.nix-gaming.packages.${pkgs.system}.proton-ge
+    # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
 
-    # games
+    ## Cli games
     _2048-in-terminal
     vitetris
     nethack
     
+    ## Celeste
     celeste-classic
     celeste-classic-pm
 
+    ## Doom
     gzdoom
     crispy-doom
 
-    # Emulation
+    ## Emulation
     # cemu
-    dolphin-emu
+    # dolphin-emu
   ];
 }
