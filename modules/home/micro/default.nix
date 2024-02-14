82fbb7d0a77a0444b0d6802dfb7be9da56dd4f55
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-14 23:07:41 +0100

    change micro config

--- modules/home/micro/default.nix
+++ modules/home/micro/default.nix
@@ -5,8 +5,8 @@
     
     settings = {
       "colorscheme" = "catppuccin-mocha";
-      "*.nix" = {"tabsize": 2};
-      "*.ml"  = {"tabsize": 2};
+      "*.nix" = {"tabsize" = 2;};
+      "*.ml"  = {"tabsize" = 2;};
       "tabstospaces" = true;
       "tabsize" = 4;
     };
