ba45a7988d839e82cbf7083d4b6ca6a1d1e16ff0
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-23 18:02:13 +0100

    change micro keybinds

--- modules/home/micro/default.nix
+++ modules/home/micro/default.nix
@@ -14,10 +14,12 @@
 
   xdg.configFile."micro/bindings.json".text = ''
     {
-      "Ctrl-Up": "CursorUp,CursorUp,CursorUp,CursorUp,CursorUp,CursorUp,CursorUp,CursorUp,CursorUp,CursorUp",
-      "Ctrl-Down": "CursorDown,CursorDown,CursorDown,CursorDown,CursorDown,CursorDown,CursorDown,CursorDown,CursorDown,CursorDown",
+      "Ctrl-Up": "CursorUp,CursorUp,CursorUp,CursorUp,CursorUp",
+      "Ctrl-Down": "CursorDown,CursorDown,CursorDown,CursorDown,CursorDown",
       "Ctrl-Backspace": "DeleteWordLeft",
-      "Ctrl-Delete": "DeleteWordRight"
+      "Ctrl-Delete": "DeleteWordRight",
+      "CtrlShiftUp": "ScrollUp,ScrollUp,ScrollUp,ScrollUp,ScrollUp",
+      "CtrlShiftDown": "ScrollDown,ScrollDown,ScrollDown,ScrollDown,ScrollDown"
     }
   '';
 
