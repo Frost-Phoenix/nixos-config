1b1550dab1827f5ce1f5a49e0d3a2f070214f11e
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-23 18:33:36 +0100

    change kitty config

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -11,12 +11,13 @@
     };
 
     settings = {
-      confirm_os_window_close = 0;
+      confirm_os_window_close = 1;
       background_opacity = "0.55";  
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
       mouse_hide_wait = 60;
+      tab_title_template = "{title}";
     };
 
     keybindings = {  
