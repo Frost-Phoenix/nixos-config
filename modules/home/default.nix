dc251a53932eb7ac47209a11468e38cae7d2a6b1
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-01 21:38:07 +0100

    remove firefox

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,29 +1,28 @@
 {inputs, username, ...}: {
   imports =
-       [(import ./waybar)]
-    ++ [(import ./firefox)]
-    ++ [(import ./floorp)]          # firefox based browser
-    ++ [(import ./vscodium)]
-    ++ [(import ./hyprland)]
-    ++ [(import ./gaming)]
-    ++ [(import ./git)]
-    ++ [(import ./btop)]
-    ++ [(import ./bat)]
-    ++ [(import ./cava)]
-    ++ [(import ./kitty)]
-    ++ [(import ./micro)]           # nano replacement
     ++ [(import ./aseprite)]        # pixel art editor
     ++ [(import ./audacious)]       # music player
-    ++ [(import ./unity)]
-    ++ [(import ./rider)]           # C# JetBrain editor
+    ++ [(import ./bat)]
+    ++ [(import ./btop)]
+    ++ [(import ./cava)]
     ++ [(import ./discord)]         # discord with catppuccin theme
-    ++ [(import ./swaylock)]
-    ++ [(import ./wofi)]
-    ++ [(import ./zsh)]
+    ++ [(import ./floorp)]          # firefox based browser
+    ++ [(import ./gaming)]
+    ++ [(import ./git)]
     ++ [(import ./gtk)]
+    ++ [(import ./hyprland)]
+    ++ [(import ./kitty)]
     ++ [(import ./mako)]            # notification deamon
+    ++ [(import ./micro)]           # nano replacement
+    ++ [(import ./nvim)]
+    ++ [(import ./package)]
+    ++ [(import ./rider)]           # C# JetBrain editor
     ++ [(import ./scripts)]         # personal scripts
     ++ [(import ./starship)]
-    ++ [(import ./nvim)]
-    ++ [(import ./package)];
+    ++ [(import ./swaylock)]
+    ++ [(import ./unity)]
+    ++ [(import ./vscodium)]
+    ++ [(import ./waybar)]
+    ++ [(import ./wofi)]
+    ++ [(import ./zsh)];
 }
