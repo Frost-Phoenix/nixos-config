7188d1c0b69a573be5512998e44eb02310bf9adb
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-06 14:52:09 +0100

    update vm script

--- modules/home/scripts/scripts/vm-start.sh
+++ modules/home/scripts/scripts/vm-start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 # VM name
-vm_name="win11"
+vm_name="win10"
 export LIBVIRT_DEFAULT_URI="qemu:///system"
 
 # change workspace
-hyprctl dispatch workspace 5
+hyprctl dispatch workspace 3
 
 virsh start ${vm_name}
 virt-viewer -f -w -a ${vm_name}
