783cafedf9b145cced551f1a169d730794f28553
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-06 14:57:07 +0100

    update vm script

--- modules/home/scripts/scripts/vm-start.sh
+++ modules/home/scripts/scripts/vm-start.sh
@@ -5,7 +5,7 @@ vm_name="win10"
 export LIBVIRT_DEFAULT_URI="qemu:///system"
 
 # change workspace
-hyprctl dispatch workspace 3
+hyprctl dispatch workspace 6
 
 virsh start ${vm_name}
 virt-viewer -f -w -a ${vm_name}
