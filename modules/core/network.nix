06a71f9c9b245891c38580a9088be0f8e47b087e
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-17 13:26:29 +0100

    change firewall config

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -6,8 +6,8 @@
     nameservers = [ "1.1.1.1" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 7777 59010 59011 ];
-      allowedUDPPorts = [ 7777 59010 59011 ];
+      allowedTCPPorts = [ 22 80 443 59010 59011 ];
+      allowedUDPPorts = [ 59010 59011 ];
       # allowedUDPPortRanges = [
         # { from = 4000; to = 4007; }
         # { from = 8000; to = 8010; }
