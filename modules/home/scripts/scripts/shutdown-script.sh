3d2ecc424d66f7911a1c7b057e730dd7f2383d13
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-21 09:49:58 +0100

    update shutdown script

--- modules/home/scripts/scripts/shutdown-script.sh
+++ modules/home/scripts/scripts/shutdown-script.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env zsh
 
-respond="$(echo "---------------- Yes ----------------\n---------------- Nah ----------------" | wofi --show dmenu)"
+respond="$(echo "---------------- Yes ----------------\n-------------- Restart --------------\n---------------- Nah ----------------" | wofi --show dmenu -k /dev/null)"
+
 if [ $respond = '---------------- Yes ----------------' ] 
 then
     echo "shutdown"
-    shutdown now
+	shutdown now    
+elif [ $respond = '-------------- Restart --------------' ] 
+then
+    echo "restart"
+    reboot
 else
     notify-send "cancel shutdown"
 fi
