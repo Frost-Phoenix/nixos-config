99beba9e6a2ff468fc5ec2f86418ec3198dd1193
Frost-Phoenix <67cyril6767@gmail.com>
2024-03-30 02:16:46 +0100

    change kitty config

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -15,7 +15,7 @@
       background_opacity = "0.55";  
       window_padding_width = 10;
       scrollback_lines = 10000;
-      enable_audio_bell = true;
+      enable_audio_bell = false;
       mouse_hide_wait = 60;
       
       ## Tabs
