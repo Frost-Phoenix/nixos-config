b5c1d7d64ec1094dd6ba09bf0688504373d998c8
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-10 22:18:23 +0200

    change hyprland config

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -77,7 +77,7 @@
           brightness = 1;
           contrast = 1.400;
           ignore_opacity = true;
-          # noise = 0.01170;
+          noise = 0;
           new_optimizations = true;
           xray = true;
         };
