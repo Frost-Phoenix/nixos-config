4bba162252d2810c97d047970f41ce22931ce560
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-12 22:07:57 +0200

    update swaylock

--- modules/home/swaylock.nix
+++ modules/home/swaylock.nix
@@ -12,12 +12,13 @@
       indicator-radius = 100;
       indicator-thickness = 7;
       
-      
       effect-blur = "7x5";
       effect-vignette = "0.5:0.5";
+      effect-pixelate = 7;
       
       color="1e1e2e";
       bs-hl-color="f5e0dc";
+      key-hl-color="a6e3a1";
       caps-lock-bs-hl-color="f5e0dc";
       caps-lock-key-hl-color="a6e3a1";
       ring-color="b4befe";
@@ -30,16 +31,15 @@
       text-caps-lock-color="fab387";
       text-ver-color="89b4fa";
       text-wrong-color="eba0ac";
+      layout-text-color="cdd6f4";
 
       inside-color="00000000";
       inside-clear-color="00000000";
       inside-caps-lock-color="00000000";
       inside-ver-color="00000000";
       inside-wrong-color="00000000";
-      key-hl-color="a6e3a1";
       layout-bg-color="00000000";
       layout-border-color="00000000";
-      layout-text-color="cdd6f4";
       line-color="00000000";
       line-clear-color="00000000";
       line-caps-lock-color="00000000";
@@ -48,36 +48,4 @@
       separator-color="00000000";
     };
   };
-
-  # services.swayidle = {
-  #   enable = true;
-  #   events = [
-  #     {
-  #       event = "before-sleep";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-  #     }
-  #     {
-  #       event = "lock";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-  #     }
-  #   ];
-  #   timeouts = [
-  #     {
-  #       timeout = 90;
-  #       command = "swaylock";
-  #     }
-  #     {
-  #       timeout = 300;
-  #       command = "systemctl suspend";
-  #     }
-  #     {
-  #       timeout = 180;
-  #       command = "systemctl suspend";
-  #       # command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
-  #       # resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
-  #     }
-  #   ];
-  # };
-
-  # systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
 }
