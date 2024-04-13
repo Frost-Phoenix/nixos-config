bd16cdde340246b1bd24c4a0cd62f15e2c43ca25
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-13 16:27:11 +0200

    change hyprland config

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -22,6 +22,9 @@
         numlock_by_default = true;
         follow_mouse = 1;
         sensitivity = 0;
+        touchpad = {
+          natural_scroll = true;
+        };
       };
 
       general = {
