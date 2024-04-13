4b3b15f240f8f174ef1d10e48b8f64dc75ecda4d
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-13 15:30:03 +0200

    add power management tool for laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -4,4 +4,53 @@
     ./hardware-configuration.nix
     ./../../modules/core
   ];
+
+  environment.systemPackages = with pkgs; [
+    acpi
+    powertop
+  ];
+  
+  hardware.trackpoint = {
+    enable = true;
+    emulateWheel = true;
+    speed = 255;
+    sensitivity = 200;
+  };
+
+  services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = true;
+ 
+    upower = {
+      enable = true;
+      percentageLow = 20;
+      percentageCritical = 5;
+      percentageAction = 3;
+      criticalPowerAction = "PowerOff";
+    };
+
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "ondemand";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+  };
+
+  boot = {
+    kernelModules = ["acpi_call"];
+    extraModulePackages = with config.boot.kernelPackages;
+      [
+        acpi_call
+        cpupower
+      ]
+      ++ [pkgs.cpupower-gui];
+  };
 }
