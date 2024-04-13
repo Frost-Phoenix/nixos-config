2873b1b7391bb699cdd549dcdc0f136756c27706
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-13 13:53:07 +0200

    add packages description

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,25 +2,25 @@
   imports =
        [(import ./aseprite/aseprite.nix)]         # pixel art editor
     ++ [(import ./audacious/audacious.nix)]       # music player
-    ++ [(import ./bat.nix)]
-    ++ [(import ./btop.nix)]
-    ++ [(import ./cava.nix)]
+    ++ [(import ./bat.nix)]                       # better cat command
+    ++ [(import ./btop.nix)]                      # resouces monitor 
+    ++ [(import ./cava.nix)]                      # audio visualizer
     ++ [(import ./discord.nix)]                   # discord with catppuccin theme
     ++ [(import ./floorp/floorp.nix)]             # firefox based browser
-    ++ [(import ./gaming.nix)]
-    ++ [(import ./git.nix)]
-    ++ [(import ./gtk.nix)]
-    ++ [(import ./hyprland)]
-    ++ [(import ./kitty.nix)]
+    ++ [(import ./gaming.nix)]                    # packages related to gaming
+    ++ [(import ./git.nix)]                       # vertion controle
+    ++ [(import ./gtk.nix)]                       # gtk theme
+    ++ [(import ./hyprland)]                      # window manager
+    ++ [(import ./kitty.nix)]                     # terminal
     ++ [(import ./mako.nix)]                      # notification deamon
     ++ [(import ./micro.nix)]                     # nano replacement
-    ++ [(import ./nvim.nix)]
-    ++ [(import ./packages.nix)]
+    ++ [(import ./nvim.nix)]                      # neovim editor
+    ++ [(import ./packages.nix)]                  # other packages
     ++ [(import ./scripts/scripts.nix)]           # personal scripts
-    ++ [(import ./starship.nix)]
-    ++ [(import ./swaylock.nix)]
-    ++ [(import ./vscodium.nix)]
-    ++ [(import ./waybar)]
-    ++ [(import ./wofi.nix)]
-    ++ [(import ./zsh.nix)];
+    ++ [(import ./starship.nix)]                  # shell prompt
+    ++ [(import ./swaylock.nix)]                  # lock screen
+    ++ [(import ./vscodium.nix)]                  # vscode forck
+    ++ [(import ./waybar)]                        # status bar
+    ++ [(import ./wofi.nix)]                      # launcher
+    ++ [(import ./zsh.nix)];                      # shell
 }
