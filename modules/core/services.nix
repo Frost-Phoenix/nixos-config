68b5ece25198223bf9cd6a4e086361ec49384fc7
Frost-Phoenix <67cyril6767@gmail.com>
2024-05-15 13:55:17 +0200

    disable power button

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -6,4 +6,8 @@
     dbus.enable = true;
     fstrim.enable = true;
   };
+  services.logind.extraConfig = ''
+    # don’t shutdown when power button is short-pressed
+    HandlePowerKey=ignore
+  '';
 }
