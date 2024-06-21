557c171fbc6261ad99871d7ba129c52a621c1850
Frost-Phoenix <67cyril6767@gmail.com>
2024-06-21 17:16:56 +0200

    fix

--- modules/home/default.desktop.nix
+++ modules/home/default.desktop.nix
@@ -1,6 +1,6 @@
 {inputs, username, host, ...}: {
   imports =
-       [(import ./default.nix)]
+       [(import ./default.nix)];
     # ++ [(import ./rider.nix)]                     # C# JetBrain editor
     # ++ [(import ./steam.nix)]
     # ++ [(import ./unity.nix)];
