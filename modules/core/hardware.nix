27247998b6e65ceb5510f4b425f5689369cd7f1c
Frost-Phoenix <67cyril6767@gmail.com>
2024-06-23 16:33:59 +0200

    update

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {  
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.enableRedistributableFirmware = true;
-  hardware.opengl.extraPackages = with pkgs; [
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
     
-  ];
+      ];
+    };
+  };
+  hardware.enableRedistributableFirmware = true;
 }
