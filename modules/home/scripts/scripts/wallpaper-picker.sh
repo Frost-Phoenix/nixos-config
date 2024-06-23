ad2feff195a233333ce8283263e91c17849d45fe
Frost-Phoenix <67cyril6767@gmail.com>
2024-06-23 15:02:30 +0200

    update wallpaper-picker script

--- modules/home/scripts/scripts/wallpaper-picker.sh
+++ modules/home/scripts/scripts/wallpaper-picker.sh
@@ -2,7 +2,7 @@
 
 wallpaper_path=$HOME/Pictures/wallpapers
 wallpapers_folder=$HOME/Pictures/wallpapers/others
-wallpaper_name="$(ls $wallpapers_folder | wofi --show dmenu --sort-order=alphabetical)"
+wallpaper_name="$(ls $wallpapers_folder | fuzzel --dmenu)"
 if [[ -f $wallpapers_folder/$wallpaper_name ]]; then
     find ~/Pictures/wallpapers -maxdepth 1 -type f -delete
     cp $wallpapers_folder/$wallpaper_name $wallpaper_path/$wallpaper_name
