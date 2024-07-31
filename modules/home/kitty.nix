e08e4e1e332a0d725ac479176f1e28b08cd38ba8
Frost-Phoenix <67cyril6767@gmail.com>
2024-07-31 21:37:14 +0200

    change theme to gruvbox

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -3,16 +3,16 @@
   programs.kitty = {
     enable = true;
 
-    theme = "Catppuccin-Mocha";
+    theme = "Gruvbox Dark Hard";
     
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 13;
+      size = 15;
     };
 
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.55";  
+      background_opacity = "1"; # "0.55";  
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
