1475b4a9f8177a8b2da5b10faa91307a47ececd0
Frost-Phoenix <67cyril6767@gmail.com>
2024-07-31 11:19:37 +0200

    change hyprland keybind

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -145,7 +145,7 @@
         "$mainMod SHIFT, Escape, exec, shutdown-script"
         "$mainMod, P, pseudo,"
         "$mainMod, J, togglesplit,"
-        "$mainMod, E, exec, nemo"
+        "$mainMod, E, exec, nautilus"
         "$mainMod SHIFT, B, exec, pkill -SIGUSR1 .waybar-wrapped"
         "$mainMod, C ,exec, hyprpicker -a"
         "$mainMod, W,exec, wallpaper-picker"
