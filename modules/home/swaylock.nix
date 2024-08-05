82678aa67a31a7848914068cc3e8b051b5f2aa27
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-05 20:29:46 +0200

    change swaylock config

--- modules/home/swaylock.nix
+++ modules/home/swaylock.nix
@@ -6,14 +6,14 @@
     settings = {
       clock = true;
       daemonize=true;
-      timestr="%H:%M";
+      # timestr="%H:%M";
       datestr = "";
       screenshots = true;
       ignore-empty-password=true;
       
       indicator = true;
       indicator-radius = 111;
-      indicator-thickness = 8;
+      indicator-thickness = 9;
       
       effect-blur = "7x5";
       effect-vignette = "0.75:0.75";
@@ -21,9 +21,9 @@
       
       font="JetBrainsMono Nerd Font";
 
-      text-wrong-color="FFFFFFFF";
-      text-ver-color="FFFFFFFF";
-      text-clear-color="FFFFFFFF";
+      text-wrong-color="FBF1C7FF";
+      text-ver-color="FBF1C7FF";
+      text-clear-color="FBF1C7FF";
       key-hl-color="fabd2fFF";
       bs-hl-color="fb4934FF";
       ring-clear-color="d65d0eFF";
@@ -35,11 +35,13 @@
       line-wrong-color="FFFFFF00";
       separator-color="FFFFFF00";
       line-color="FFFFFF00";
-      text-color="FFFFFFFF";
-      inside-color="3C3836CC";
-      inside-ver-color="3C3836CC";
-      inside-clear-color="3C3836CC";
-      inside-wrong-color="3C3836CC";
+      text-color="FBF1C7FF";
+      inside-color="3C3836DD";
+      inside-ver-color="3C3836DD";
+      inside-clear-color="3C3836DD";
+      inside-wrong-color="3C3836DD";
+      layout-bg-color = "FFFFFF00";
+      layout-text-color = "FBF1C7FF";
     };
   };
 }
