60ee584432882f55c8cab570dbd71a5af0f2688f
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-11 20:53:19 +0200

    change hyprlock config

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
+let 
+  text = "rgb(251, 241, 199)";
+in
 {
   home.packages = [ pkgs.hyprlock ];
   xdg.configFile."hypr/hyprlock.conf".text = ''
     # BACKGROUND
     background {
       monitor =
-      path = ~/Pictures/wallpapers/others/gruv_spac.jpg
+      path = ~/Pictures/wallpapers/others/gruv_forest.jpg
       blur_passes = 1
       contrast = 0.8916
       brightness = 0.8172
@@ -23,35 +26,23 @@
     # Day
     label {
       monitor =
-      text = cmd[update:1000] echo -e "$(date +"%A")"
-      color = rgba(251, 241, 199, 0.85)
-      font_size = 90
-      font_family = SF Pro Display Bold
-      position = 0, 360
-      halign = center
-      valign = center
-    }
-
-    # Date-Month
-    label {
-      monitor =
-      text = cmd[update:1000] echo -e "$(date +"%d %B")"
-      color = rgba(251, 241, 199, 0.85)
-      font_size = 40
-      font_family = SF Pro Display Bold
-      position = 0, 260
+      text = cmd[update:1000] echo "- $(date +"%A, %B %d") -"
+      color = rgba(242, 243, 244, 0.75)
+      font_size = 20
+      font_family = JetBrainsMono Nerd Font
+      position = 0, 190
       halign = center
       valign = center
     }
 
     # Time
     label {
-      monitor =
-      text = cmd[update:1000] echo "<span>$(date +"- %H:%M -")</span>"
-      color = rgba(251, 241, 199, 0.85)
-      font_size = 20
-      font_family = SF Pro Display Bold
-      position = 0, 200
+      monitor = 
+      text = cmd[update:1000] echo "$(date +"%k:%M")"
+      color = rgba(242, 243, 244, 0.75)
+      font_size = 111
+      font_family = JetBrainsMono Nerd Font
+      position = 0, 300
       halign = center
       valign = center
     }
@@ -59,9 +50,9 @@
     # USER-BOX
     shape {
       monitor =
-      size = 300, 60
+      size = 350, 50
       color = rgba(255, 255, 255, .2)
-      rounding = -1
+      rounding = 15
       border_size = 0
       border_color = rgba(255, 255, 255, 0)
       rotate = 0
@@ -74,14 +65,14 @@
     # USER
     label {
       monitor =
-      text =     $USER
+      text =   $USER
       color = rgba(216, 222, 233, 0.80)
       outline_thickness = 2
       dots_size = 0.25 # Scale of input-field height, 0.2 - 0.8
       dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
       dots_center = true
-      font_size = 18
-      font_family = SF Pro Display Bold
+      font_size = 16
+      font_family = JetBrainsMono Nerd Font
       position = 0, -190
       halign = center
       valign = center
@@ -90,19 +81,20 @@
     # INPUT FIELD
     input-field {
       monitor =
-      size = 300, 60
-      outline_thickness = 2
-      dots_size = 0.22 # Scale of input-field height, 0.2 - 0.8
+      size = 350, 50
+      outline_thickness = 0
+      rounding = 15
+      dots_size = 0.25 # Scale of input-field height, 0.2 - 0.8
       dots_spacing = 0.4 # Scale of dots' absolute size, 0.0 - 1.0
       dots_center = true
       outer_color = rgba(255, 255, 255, 0)
       inner_color = rgba(255, 255, 255, 0.2)
       font_color = rgb(200, 200, 200)
       fade_on_empty = false
-      font_family = SF Pro Display Bold
+      font_family = JetBrainsMono Nerd Font
       placeholder_text = <i><span foreground="##ffffff99">Enter Password</span></i>
       hide_input = false
-      position = 0, -270
+      position = 0, -260
       halign = center
       valign = center
     }
