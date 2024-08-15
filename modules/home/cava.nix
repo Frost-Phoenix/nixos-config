5968336ecbfe4ff76142850c4e5f0f616da9226d
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-15 11:54:04 +0200

    change cava config

--- modules/home/cava.nix
+++ modules/home/cava.nix
@@ -7,16 +7,21 @@
   home.file.".config/cava/config".text = ''
     # custom cava config
 
+    [general]
+    autosens = 1
+    overshoot = 0
+
     [color]
     gradient = 1
-
-    gradient_color_1 = '#98971a'
-    gradient_color_2 = '#98971a'
-    gradient_color_3 = '#b8bb26'
-    gradient_color_4 = '#b8bb26'
-    gradient_color_5 = '#d79921'
-    gradient_color_6 = '#d79921'
-    gradient_color_7 = '#fb4934'
-    gradient_color_8 = '#fb4934'
+    gradient_count = 8
+    
+    gradient_color_1 = '#99991a'
+    gradient_color_2 = '#a28e00'
+    gradient_color_3 = '#ab8200'
+    gradient_color_4 = '#b37400'
+    gradient_color_5 = '#bb6600'
+    gradient_color_6 = '#c25400'
+    gradient_color_7 = '#c8400d'
+    gradient_color_8 = '#cd231d'
   '';
 }
