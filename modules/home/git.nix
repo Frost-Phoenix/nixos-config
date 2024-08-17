eb34d581cdc782b57443f3c7b05430bafabdd07a
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-17 15:03:55 +0200

    add delta to git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -9,6 +9,17 @@
     extraConfig = { 
       init.defaultBranch = "main";
       credential.helper = "store";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+    };
+
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = true;
+        navigate = true;
+      };
     };
   };
 
