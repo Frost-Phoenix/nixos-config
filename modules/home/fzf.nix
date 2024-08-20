7b3ef9f2ba0c2f27a2b03ba0a5fa0eb1030c627b
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-20 17:47:17 +0200

    change fzf config

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -15,7 +15,7 @@
       "--color=fg:-1,fg+:#FBF1C7,bg:-1,bg+:#282828"
       "--color=hl:#98971A,hl+:#B8BB26,info:#928374,marker:#D65D0E"
       "--color=prompt:#CC241D,spinner:#689D6A,pointer:#D65D0E,header:#458588"
-      "--color=border:#3C3836,label:#aeaeae,query:#FBF1C7"
+      "--color=border:#665C54,label:#aeaeae,query:#FBF1C7"
       "--border='rounded' --border-label='' --preview-window='border-rounded' --prompt='> '"
       "--marker='>' --pointer='>' --separator='─' --scrollbar='│'"
       "--info='right'"
