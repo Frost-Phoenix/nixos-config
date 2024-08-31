cab88d020302171f80c8427b21e7ba33e154ae83
Frost-Phoenix <67cyril6767@gmail.com>
2024-08-31 14:24:41 +0200

    add google dns servers

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,15 +3,11 @@
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    nameservers = [ "1.1.1.1" ];
+    nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 80 443 59010 59011 ];
       allowedUDPPorts = [ 59010 59011 ];
-      # allowedUDPPortRanges = [
-        # { from = 4000; to = 4007; }
-        # { from = 8000; to = 8010; }
-      # ];
     };
   };
 
