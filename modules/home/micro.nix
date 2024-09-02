6cdd9f14c93803b5c8a0ab6692d74888cce68d6b
Frost-Phoenix <67cyril6767@gmail.com>
2024-09-02 18:39:31 +0200

    add config for makefile

--- modules/home/micro.nix
+++ modules/home/micro.nix
@@ -8,6 +8,7 @@
       "*.nix" = {"tabsize" = 2;};
       "*.ml"  = {"tabsize" = 2;};
       "*.sh"  = {"tabsize" = 2;};
+      "makefile" = {"tabstospaces" = false;};
       "tabstospaces" = true;
       "tabsize" = 4;
       "mkparents" = true;
