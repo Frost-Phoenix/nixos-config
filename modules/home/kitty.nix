9825a8a759de48c02dfe7a213e09542cb4598d0c
Frost-Phoenix <67cyril6767@gmail.com>
2024-09-23 21:42:31 +0200

    change kitty config

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -13,7 +13,7 @@
     settings = {
       confirm_os_window_close = 0;
       background_opacity = "0.75";
-      window_padding_width = 10;
+      window_padding_width = 2;
       scrollback_lines = 10000;
       enable_audio_bell = false;
       mouse_hide_wait = 60;
