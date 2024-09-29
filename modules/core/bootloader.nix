2dec9d195d59b9abf8b984dc431150c276c0b740
EloToJaa <elotoja@protonmail.com>
2024-09-29 14:29:21 +0200

    Update systemd boot loader

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    efi.canTouchEfiVariables = true;
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
