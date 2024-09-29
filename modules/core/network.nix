3606234ffddf91a42ed9e0c991d21ebbe5b8ce1a
EloToJaa <elotoja@protonmail.com>
2024-09-29 12:51:38 +0200

    Add monitors

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,7 +3,7 @@
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
+    nameservers = [ "192.168.0.31" "1.1.1.1" ];
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 80 443 59010 59011 ];
