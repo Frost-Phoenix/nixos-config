e42806f60befe7b6a5a07b031867b9b210c9eeda
EloToJaa <elotoja@protonmail.com>
2024-09-29 16:48:48 +0200

    Update monitors

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -322,9 +322,9 @@
     };
 
     extraConfig = "
-      monitor=DP-1, 1920x1080@60, 3840x0, 1
-      monitor=DP-2, 1920x1080@165, 1920x0, 1
       monitor=HDMI-A-1, 1920x1080@60, 0x0, 1
+      monitor=DP-2, 1920x1080@165, 1920x0, 1
+      monitor=DP-1, 1920x1080@60, 3840x0, 1
 
       xwayland {
         force_zero_scaling = true
