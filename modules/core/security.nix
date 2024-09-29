634e13f60fb79988dcb393f4b305f9345933f25b
EloToJaa <elotoja@protonmail.com>
2024-09-29 19:09:06 +0200

    Fix brackets

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -3,17 +3,13 @@
   security.rtkit.enable = true;
   security.sudo = {
     enable = true;
-    extraRules = [
-      {
-        users = [ "${username}" ];
-        commands = [
-          {
-            command = "ALL";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-      }
-    ];
+    extraRules = [{
+      users = [ "${username}" ];
+      commands = [{
+        command = "ALL";
+        options = [ "NOPASSWD" ];
+      }];
+    }];
   };
   security.pam.services.hyprlock = {};
 }
