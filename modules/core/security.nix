13ce1d3e80f36d83974d5b3cf1d12307b827afc9
EloToJaa <elotoja@protonmail.com>
2024-09-29 18:49:36 +0200

    Add sudo extraRules

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,7 +1,15 @@
 { ... }: 
 {
   security.rtkit.enable = true;
-  security.sudo.enable = true;
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        users = [ ${username} ];
+        options = [ "NOPASSWD" ];
+      }
+    ];
+  };
   # security.pam.services.swaylock = { };
   security.pam.services.hyprlock = {};
 }
