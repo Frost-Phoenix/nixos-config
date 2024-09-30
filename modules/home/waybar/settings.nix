815fa9748b298ae54c199b5982fb85e64d967360
Frost-Phoenix <67cyril6767@gmail.com>
2024-09-30 23:17:03 +0200

    update waybar config

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -59,15 +59,16 @@ in
         format = "{icon}";
         on-click= "activate";
         format-icons= {
-            "1"= "I";
-            "2"= "II";
-            "3"= "III";
-            "4"= "IV";
-            "5"= "V";
-            "6"= "VI";
-            "7"= "VII";
-            "8"= "VII";
-            "9"= "IX";
+            "1"  = "I";
+            "2"  = "II";
+            "3"  = "III";
+            "4"  = "IV";
+            "5"  = "V";
+            "6"  = "VI";
+            "7"  = "VII";
+            "8"  = "VII";
+            "9"  = "IX";
+            "10" = "X";
             sort-by-number= true;
         };
         persistent-workspaces = {
