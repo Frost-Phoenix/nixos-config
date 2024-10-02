8517baf2097d1996f2c247fc7a264e871f4e0dd2
Frost-Phoenix <67cyril6767@gmail.com>
2024-10-02 10:08:58 +0200

    change hyprland animation

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -112,24 +112,26 @@
           "fluent_decel, 0, 0.2, 0.4, 1"
           "easeOutCirc, 0, 0.55, 0.45, 1"
           "easeOutCubic, 0.33, 1, 0.68, 1"
-          "easeinoutsine, 0.37, 0, 0.63, 1"
+          "fade_curve, 0, 0.55, 0.45, 1"
         ];
 
         animation = [
+          # name, enable, speed, curve, style
+        
           # Windows
-          "windowsIn, 1, 4, easeOutCubic, popin 20%" # window open
-          "windowsOut, 1, 4, fluent_decel, popin 80%" # window close.
-          "windowsMove, 1, 2, easeinoutsine, slide" # everything in between, moving, dragging, resizing.
+          "windowsIn,   0, 4, easeOutCubic,  popin 20%" # window open
+          "windowsOut,  0, 4, fluent_decel,  popin 80%" # window close.
+          "windowsMove, 1, 2, fluent_decel, slide"     # everything in between, moving, dragging, resizing.
 
           # Fade
-          "fadeIn, 1, 3, easeOutCubic" # fade in (open) -> layers and windows
-          "fadeOut, 1, 2, easeOutCubic" # fade out (close) -> layers and windows
-          "fadeSwitch, 0, 1, easeOutCirc" # fade on changing activewindow and its opacity
-          "fadeShadow, 1, 10, easeOutCirc" # fade on changing activewindow for shadows
-          "fadeDim, 1, 4, fluent_decel" # the easing of the dimming of inactive windows
-          "border, 1, 2.7, easeOutCirc" # for animating the border's color switch speed
-          "borderangle, 1, 30, fluent_decel, once" # for animating the border's gradient angle - styles: once (default), loop
-          "workspaces, 1, 5, easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
+          "fadeIn,      1, 4,   fade_curve"   # fade in (open) -> layers and windows
+          "fadeOut,     1, 4,   fade_curve"   # fade out (close) -> layers and windows
+          "fadeSwitch,  0, 1,   easeOutCirc"  # fade on changing activewindow and its opacity
+          "fadeShadow,  1, 10,  easeOutCirc"  # fade on changing activewindow for shadows
+          "fadeDim,     1, 4,   fluent_decel" # the easing of the dimming of inactive windows
+          "border,      1, 2.7, easeOutCirc"  # for animating the border's color switch speed
+          "borderangle, 1, 30,  fluent_decel, once" # for animating the border's gradient angle - styles: once (default), loop
+          "workspaces,  1, 3,   easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
         ];
       };
 
