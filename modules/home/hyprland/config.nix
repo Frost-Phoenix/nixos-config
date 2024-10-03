eb04139bb6ff6a64e401d14682d842b9c5973a5d
Frost-Phoenix <67cyril6767@gmail.com>
2024-10-03 16:17:31 +0200

    change hyprland config

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -124,14 +124,14 @@
           "windowsMove, 1, 2, fluent_decel, slide"     # everything in between, moving, dragging, resizing.
 
           # Fade
-          "fadeIn,      1, 4,   fade_curve"   # fade in (open) -> layers and windows
-          "fadeOut,     1, 4,   fade_curve"   # fade out (close) -> layers and windows
+          "fadeIn,      1, 3,   fade_curve"   # fade in (open) -> layers and windows
+          "fadeOut,     1, 3,   fade_curve"   # fade out (close) -> layers and windows
           "fadeSwitch,  0, 1,   easeOutCirc"  # fade on changing activewindow and its opacity
           "fadeShadow,  1, 10,  easeOutCirc"  # fade on changing activewindow for shadows
           "fadeDim,     1, 4,   fluent_decel" # the easing of the dimming of inactive windows
           "border,      1, 2.7, easeOutCirc"  # for animating the border's color switch speed
           "borderangle, 1, 30,  fluent_decel, once" # for animating the border's gradient angle - styles: once (default), loop
-          "workspaces,  1, 3,   easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
+          "workspaces,  1, 4,   easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
         ];
       };
 
@@ -149,7 +149,7 @@
         "$mainMod SHIFT, F, fullscreen, 1"
         "$mainMod, Space, exec, toggle_float"
         "$mainMod, D, exec, rofi -show drun || pkill rofi"
-        "$mainMod SHIFT, D, exec, webcord"
+        "$mainMod SHIFT, D, exec, webcord --enable-features=UseOzonePlatform --ozone-platform=wayland"
         "$mainMod SHIFT, S, exec, hyprctl dispatch exec '[workspace 5 silent] SoundWireServer'"
         "$mainMod, Escape, exec, swaylock"
         "ALT, Escape, exec, hyprlock"
