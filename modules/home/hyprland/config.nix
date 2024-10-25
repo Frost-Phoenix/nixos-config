4cf99eff4aaf15539207d2f55465cf2705f1c8b0
Frost-Phoenix <67cyril6767@gmail.com>
2024-10-25 23:04:28 +0200

    change hyprland border

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -45,7 +45,7 @@
         gaps_out = 10;
         border_size = 2;
         "col.active_border" = "rgb(98971A)";
-        "col.inactive_border" = "rgb(928374)";
+        "col.inactive_border" = "0x00000000";
         border_part_of_window = false;
         no_border_on_floating = false;
       };
