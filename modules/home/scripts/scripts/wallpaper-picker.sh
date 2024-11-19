b4f7064dc647e0d02c9efe73f55d9dc008643b7b
Frost-Phoenix <67cyril6767@gmail.com>
2024-11-19 16:53:58 +0100

    update wallpaper-picker script

--- modules/home/scripts/scripts/wallpaper-picker.sh
+++ modules/home/scripts/scripts/wallpaper-picker.sh
@@ -3,10 +3,10 @@
 wallpaper_path=$HOME/Pictures/wallpapers
 wallpapers_folder=$HOME/Pictures/wallpapers/others
 wallpaper_name="$(ls $wallpapers_folder | rofi -dmenu || pkill rofi)"
+
 if [[ -f $wallpapers_folder/$wallpaper_name ]]; then
-    find ~/Pictures/wallpapers -maxdepth 1 -type f -delete
-    cp $wallpapers_folder/$wallpaper_name $wallpaper_path/$wallpaper_name
-    wall-change $wallpaper_path/$wallpaper_name
+    ln -sf "$wallpapers_folder/$wallpaper_name" "$wallpaper_path/wallpaper"
+    wall-change "$wallpaper_path/wallpaper"
 else
     exit 1
 fi
