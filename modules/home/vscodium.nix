4a74d53c734b22263274ecf64842e4085ebeb0a7
Frost-Phoenix <67cyril6767@gmail.com>
2024-11-24 10:43:26 +0100

    fmt: apply formatting

--- modules/home/vscodium.nix
+++ modules/home/vscodium.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 let
-  jonathanharty.gruvbox-material-icon-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "gruvbox-material-icon-theme";
-      publisher = "JonathanHarty";
-      version = "1.1.5";
-      hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
-    };
-  };
+  jonathanharty.gruvbox-material-icon-theme =
+    pkgs.vscode-utils.buildVscodeMarketplaceExtension
+      {
+        mktplcRef = {
+          name = "gruvbox-material-icon-theme";
+          publisher = "JonathanHarty";
+          version = "1.1.5";
+          hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
+        };
+      };
 in
 {
   programs.vscode = {
