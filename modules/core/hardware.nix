9f9ea0d7e3a69c652ce707f7fe7d8ba3bb7edaf3
Frost-Phoenix <67cyril6767@gmail.com>
2024-12-01 13:40:00 +0100

    pgks: add hardware pkgs

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -4,7 +4,10 @@
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
-
+        intel-media-driver
+        (vaapiIntel.override { enableHybridCodec = true; })
+        vaapiVdpau
+        libvdpau-va-gl
       ];
     };
   };
