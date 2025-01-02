9e823813a88c114511425aa9f0477552a711b1a5
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-02 22:43:12 +0100

    core: test gnome config

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,10 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services = {
     gvfs.enable = true;
-    gnome.gnome-keyring.enable = true;
+    gnome = {
+      tinysparql.enable = true;
+      gnome-keyring.enable = true;
+    };
     dbus.enable = true;
     fstrim.enable = true;
+
+    # needed for GNOME services outside of GNOME Desktop
+    dbus.packages = with pkgs; [
+      gcr
+      gnome-settings-daemon
+    ];
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
