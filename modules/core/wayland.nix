dfbb0ffaa501a2dadb46ce6ec47eeeebc1ead45f
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-02 22:43:50 +0100

    core: try different hyprland pkgs

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -1,17 +1,21 @@
 { inputs, pkgs, ... }:
 {
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.default;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  };
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     xdgOpenUsePortal = true;
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
+      # inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
     ];
   };
-
-  environment.systemPackages = with pkgs; [
-    # xwaylandvideobridge
-  ];
 }
