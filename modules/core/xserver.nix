802b04983dd2e364d132ed703a94fb49c82e7cbf
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-02 22:46:35 +0100

    pkgs: remove un-used stuff

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -12,9 +12,6 @@
     };
     libinput = {
       enable = true;
-      # mouse = {
-      #   accelProfile = "flat";
-      # };
     };
   };
   # To prevent getting stuck at shutdown
--- modules/home/xdg-mimes.nix
+++ modules/home/xdg-mimes.nix
@@ -94,8 +94,6 @@ in
   xdg.mimeApps.associations.added = associations;
   xdg.mimeApps.defaultApplications = associations;
 
-  home.packages = with pkgs; [ junction ];
-
   home.sessionVariables = {
     # prevent wine from creating file associations
     WINEDLLOVERRIDES = "winemenubuilder.exe=d";
