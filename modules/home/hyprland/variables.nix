814669953b61ad66f1779af1831a300def61b5bf
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-02 22:44:57 +0100

    config: add XDG env variables

--- modules/home/hyprland/variables.nix
+++ modules/home/hyprland/variables.nix
@@ -20,7 +20,9 @@
     WLR_BACKEND = "vulkan";
     WLR_RENDERER = "vulkan";
     WLR_NO_HARDWARE_CURSORS = "1";
+    XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
     SDL_VIDEODRIVER = "wayland";
     CLUTTER_BACKEND = "wayland";
     GTK_THEME = "Gruvbox-Green-Dark";
