4947e8f12d7aa1c0c7c1863681d665e4dc9fff3b
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-04 18:43:56 +0100

    fix: zen-browser mime type

--- modules/home/xdg-mimes.nix
+++ modules/home/xdg-mimes.nix
@@ -2,7 +2,7 @@
 with lib;
 let
   defaultApps = {
-    browser = [ "zen-browser.desktop" ];
+    browser = [ "zen.desktop" ];
     text = [ "org.gnome.TextEditor.desktop" ];
     image = [ "imv-dir.desktop" ];
     audio = [ "mpv.desktop" ];
