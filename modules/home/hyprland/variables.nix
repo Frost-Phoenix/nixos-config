8a654164581f8f58d747b4737e6cb572d39c61f8
Frost-Phoenix <67cyril6767@gmail.com>
2025-01-18 11:25:03 +0100

    config: test

--- modules/home/hyprland/variables.nix
+++ modules/home/hyprland/variables.nix
@@ -1,25 +1,25 @@
 { ... }:
 {
   home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    __GL_GSYNC_ALLOWED = "0";
-    __GL_VRR_ALLOWED = "0";
-    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    NIXOS_OZONE_WL = 1;
+    __GL_GSYNC_ALLOWED = 0;
+    __GL_VRR_ALLOWED = 0;
+    _JAVA_AWT_WM_NONEREPARENTING = 1;
     SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
-    DISABLE_QT5_COMPAT = "0";
+    DISABLE_QT5_COMPAT = 0;
     GDK_BACKEND = "wayland";
-    ANKI_WAYLAND = "1";
+    ANKI_WAYLAND = 1;
     DIRENV_LOG_FORMAT = "";
-    WLR_DRM_NO_ATOMIC = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    WLR_DRM_NO_ATOMIC = 1;
+    QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
     QT_QPA_PLATFORM = "xcb";
     QT_QPA_PLATFORMTHEME = "qt5ct";
     QT_STYLE_OVERRIDE = "kvantum";
-    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_ENABLE_WAYLAND = 1;
     WLR_BACKEND = "vulkan";
     WLR_RENDERER = "vulkan";
-    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_NO_HARDWARE_CURSORS = 1;
     XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
     XDG_SESSION_DESKTOP = "Hyprland";
