19d32c9716734bdceb73fdc5984573b91eea2236
Frost-Phoenix <67cyril6767@gmail.com>
2025-02-14 13:02:00 +0100

    core: add ntfs support

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,4 +4,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = [ "ntfs" ];
 }
