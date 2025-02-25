45cb788b6470e2a8a476e5f7a832723d08fc7786
Frost-Phoenix <67cyril6767@gmail.com>
2025-02-25 17:22:36 +0100

    core: enable cachix for hyprland and ghostty

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -14,9 +14,15 @@
         "nix-command"
         "flakes"
       ];
-      substituters = [ "https://nix-gaming.cachix.org" ];
+      substituters = [
+        "https://nix-gaming.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://ghostty.cachix.org"
+      ];
       trusted-public-keys = [
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
     };
   };
