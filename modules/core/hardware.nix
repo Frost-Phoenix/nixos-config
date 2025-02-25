f089d1b9e4deac1199e457e2c802bc03846b54e3
Frost-Phoenix <67cyril6767@gmail.com>
2025-02-25 17:23:01 +0100

    core: use mesa pkg provided by hyprland

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
+let
+  hyprland-pkgs =
+    inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   hardware = {
     graphics = {
       enable = true;
+      package = hyprland-pkgs.mesa.drivers;
       extraPackages = with pkgs; [
         intel-media-driver
         (vaapiIntel.override { enableHybridCodec = true; })
