14b4af4bd4d71a63aab53aaaffd70901aeec86ab
Frost-Phoenix <67cyril6767@gmail.com>
2025-02-28 02:02:16 +0100

    pgks: hyprland test

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -19,6 +19,9 @@
   ];
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.default;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
     xwayland = {
       enable = true;
       # hidpi = true;
