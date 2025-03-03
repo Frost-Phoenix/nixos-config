5e0d00fd5e3ca73366c25a5644c44bde59040558
Frost-Phoenix <67cyril6767@gmail.com>
2025-03-03 13:28:02 +0100

    update: fix breaking change

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -42,7 +42,7 @@
         border_size = 2;
         "col.active_border" = "rgb(98971A) rgb(CC241D) 45deg";
         "col.inactive_border" = "0x00000000";
-        border_part_of_window = false;
+        # border_part_of_window = false;
         no_border_on_floating = false;
       };
 
