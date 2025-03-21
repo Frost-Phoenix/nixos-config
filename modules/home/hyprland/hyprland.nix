0ec47c562a6d9b863b507b04a9abd61bf4cd6ab9
Frost-Phoenix <67cyril6767@gmail.com>
2025-03-21 11:21:57 +0100

    fix: using both hyprland nixos and hm module

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -18,9 +18,9 @@
   ];
   wayland.windowManager.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.default;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+    package = null;
+    portalPackage = null;
+
     xwayland = {
       enable = true;
       # hidpi = true;
