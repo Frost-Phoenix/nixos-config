1bbf36327860357bda760c69a83f7b0ca535cd38
Frost-Phoenix <67cyril6767@gmail.com>
2025-04-13 22:44:04 +0200

    pkgs: vscodium: replace microsoft extention by clangd

--- modules/home/vscodium.nix
+++ modules/home/vscodium.nix
@@ -10,16 +10,14 @@ let
           hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
         };
       };
-    ziglang_vscode-zig =
-      pkgs.vscode-utils.buildVscodeMarketplaceExtension
-        {
-          mktplcRef = {
-            name = "vscode-zig";
-            publisher = "ziglang";
-            version = "0.6.5";
-            hash = "sha256-eFfucWSioF1w4veoO8VAFNi5q2g9JZbZu+NEOuuyHtM=";
-          };
-        };
+  ziglang_vscode-zig = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-zig";
+      publisher = "ziglang";
+      version = "0.6.5";
+      hash = "sha256-eFfucWSioF1w4veoO8VAFNi5q2g9JZbZu+NEOuuyHtM=";
+    };
+  };
 
 in
 {
@@ -35,7 +33,7 @@ in
         # python
         # ms-python.python
         # C/C++
-        ms-vscode.cpptools
+        llvm-vs-code-extensions.vscode-clangd
         # OCaml
         # ocamllabs.ocaml-platform
         # Zig
@@ -68,6 +66,7 @@ in
         "editor.formatOnSave" = true;
         "editor.formatOnType" = true;
         "editor.formatOnPaste" = true;
+        "editor.inlayHints.enabled" = "off";
 
         "workbench.layoutControl.type" = "menu";
         "workbench.editor.limit.enabled" = true;
@@ -88,28 +87,11 @@ in
 
         "editor.mouseWheelZoom" = true;
 
-        "C_Cpp.autocompleteAddParentheses" = true;
-        "C_Cpp.formatting" = "clangFormat";
-        "C_Cpp.vcFormat.newLine.closeBraceSameLine.emptyFunction" = true;
-        "C_Cpp.vcFormat.newLine.closeBraceSameLine.emptyType" = true;
-        "C_Cpp.vcFormat.space.beforeEmptySquareBrackets" = true;
-        "C_Cpp.vcFormat.newLine.beforeOpenBrace.block" = "sameLine";
-        "C_Cpp.vcFormat.newLine.beforeOpenBrace.function" = "sameLine";
-        "C_Cpp.vcFormat.newLine.beforeElse" = false;
-        "C_Cpp.vcFormat.newLine.beforeCatch" = false;
-        "C_Cpp.vcFormat.newLine.beforeOpenBrace.type" = "sameLine";
-        "C_Cpp.vcFormat.space.betweenEmptyBraces" = true;
-        "C_Cpp.vcFormat.space.betweenEmptyLambdaBrackets" = true;
-        "C_Cpp.vcFormat.indent.caseLabels" = true;
-        "C_Cpp.intelliSenseCacheSize" = 2048;
-        "C_Cpp.intelliSenseMemoryLimit" = 2048;
-        "C_Cpp.default.browse.path" = [ ''''${workspaceFolder}/**'' ];
-        "C_Cpp.default.cStandard" = "gnu11";
-        "C_Cpp.inlayHints.parameterNames.hideLeadingUnderscores" = false;
-        "C_Cpp.intelliSenseUpdateDelay" = 500;
-        "C_Cpp.workspaceParsingPriority" = "medium";
-        "C_Cpp.clang_format_sortIncludes" = true;
-        "C_Cpp.doxygen.generatedStyle" = "/**";
+        # C/C++
+        "clangd.arguments" = [
+          "--clang-tidy"
+          "--inlay-hints=false"
+        ];
 
         # Zig
         "zig.initialSetupDone" = true;
