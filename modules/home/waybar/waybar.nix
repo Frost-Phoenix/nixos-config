1f6222bc7186fb6f148e998973ef84cd2893c295
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-11 00:35:37 +0200

    config: waybar: used default pkg

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -3,7 +3,4 @@
   programs.waybar = {
     enable = true;
   };
-  programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
-    mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
-  });
 }
