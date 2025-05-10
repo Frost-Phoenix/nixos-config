54055056895003456016f472ec59d7bfa275fe33
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-11 01:54:16 +0200

    refactor: dynamically build scripts

--- modules/home/scripts/scripts.nix
+++ modules/home/scripts/scripts.nix
@@ -1,97 +1,26 @@
 { pkgs, ... }:
 let
-  wall-change = pkgs.writeShellScriptBin "wall-change" (
-    builtins.readFile ./scripts/wall-change.sh
-  );
-  wallpaper-picker = pkgs.writeShellScriptBin "wallpaper-picker" (
-    builtins.readFile ./scripts/wallpaper-picker.sh
-  );
-  random-wallpaper = pkgs.writeShellScriptBin "random-wallpaper" (
-    builtins.readFile ./scripts/random-wallpaper.sh
+  scriptDir = ./scripts;
+  scriptEntries = builtins.readDir scriptDir;
+
+  regularFiles = builtins.filter (name: scriptEntries.${name} == "regular") (
+    builtins.attrNames scriptEntries
   );
 
-  runbg = pkgs.writeShellScriptBin "runbg" (builtins.readFile ./scripts/runbg.sh);
-  music = pkgs.writeShellScriptBin "music" (builtins.readFile ./scripts/music.sh);
-  lofi = pkgs.writeScriptBin "lofi" (builtins.readFile ./scripts/lofi.sh);
+  shellScripts = builtins.filter (
+    name: builtins.match ".*\\.sh$" name != null
+  ) regularFiles;
 
-  toggle_blur = pkgs.writeScriptBin "toggle_blur" (
-    builtins.readFile ./scripts/toggle_blur.sh
-  );
-  toggle_oppacity = pkgs.writeScriptBin "toggle_oppacity" (
-    builtins.readFile ./scripts/toggle_oppacity.sh
-  );
-  toggle_waybar = pkgs.writeScriptBin "toggle_waybar" (
-    builtins.readFile ./scripts/toggle_waybar.sh
-  );
-  toggle_float = pkgs.writeScriptBin "toggle_float" (
-    builtins.readFile ./scripts/toggle_float.sh
-  );
+  mkScript = name: {
+    name = name;
+    value = pkgs.writeShellScriptBin (builtins.replaceStrings [ ".sh" ] [ "" ]
+      name
+    ) (builtins.readFile (scriptDir + "/${name}"));
+  };
 
-  maxfetch = pkgs.writeScriptBin "maxfetch" (
-    builtins.readFile ./scripts/maxfetch.sh
-  );
-
-  compress = pkgs.writeScriptBin "compress" (
-    builtins.readFile ./scripts/compress.sh
-  );
-  extract = pkgs.writeScriptBin "extract" (
-    builtins.readFile ./scripts/extract.sh
-  );
-
-  show-keybinds = pkgs.writeScriptBin "show-keybinds" (
-    builtins.readFile ./scripts/keybinds.sh
-  );
-
-  vm-start = pkgs.writeScriptBin "vm-start" (
-    builtins.readFile ./scripts/vm-start.sh
-  );
-
-  ascii = pkgs.writeScriptBin "ascii" (builtins.readFile ./scripts/ascii.sh);
-
-  record = pkgs.writeScriptBin "record" (builtins.readFile ./scripts/record.sh);
-
-  screenshot = pkgs.writeScriptBin "screenshot" (
-    builtins.readFile ./scripts/screenshot.sh
-  );
-
-  rofi-power-menu = pkgs.writeScriptBin "rofi-power-menu" (
-    builtins.readFile ./scripts/rofi-power-menu.sh
-  );
-  power-menu = pkgs.writeScriptBin "power-menu" (
-    builtins.readFile ./scripts/power-menu.sh
-  );
+  scriptsSet = builtins.listToAttrs (map mkScript shellScripts);
+  scripts = builtins.attrValues scriptsSet;
 in
 {
-  home.packages = [
-    wall-change
-    wallpaper-picker
-    random-wallpaper
-
-    runbg
-    music
-    lofi
-
-    toggle_blur
-    toggle_oppacity
-    toggle_waybar
-    toggle_float
-
-    maxfetch
-
-    compress
-    extract
-
-    show-keybinds
-
-    vm-start
-
-    ascii
-
-    record
-
-    screenshot
-
-    rofi-power-menu
-    power-menu
-  ];
+  home.packages = scripts;
 }
