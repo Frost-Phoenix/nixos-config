1d90659af8807a2c615409f34b0f811b069ecc23
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-11 00:34:46 +0200

    config: cava: use home-manager options

--- modules/home/cava.nix
+++ modules/home/cava.nix
@@ -2,26 +2,26 @@
 {
   programs.cava = {
     enable = true;
+
+    settings = {
+      general = {
+        autosens = 1;
+        overshoot = 0;
+      };
+
+      color = {
+        gradient = 1;
+        gradient_count = 8;
+
+        gradient_color_1 = "'#99991a'";
+        gradient_color_2 = "'#a28e00'";
+        gradient_color_3 = "'#ab8200'";
+        gradient_color_4 = "'#b37400'";
+        gradient_color_5 = "'#bb6600'";
+        gradient_color_6 = "'#c25400'";
+        gradient_color_7 = "'#c8400d'";
+        gradient_color_8 = "'#cd231d'";
+      };
+    };
   };
-
-  xdg.configFile."cava/config".text = ''
-    # custom cava config
-
-    [general]
-    autosens = 1
-    overshoot = 0
-
-    [color]
-    gradient = 1
-    gradient_count = 8
-
-    gradient_color_1 = '#99991a'
-    gradient_color_2 = '#a28e00'
-    gradient_color_3 = '#ab8200'
-    gradient_color_4 = '#b37400'
-    gradient_color_5 = '#bb6600'
-    gradient_color_6 = '#c25400'
-    gradient_color_7 = '#c8400d'
-    gradient_color_8 = '#cd231d'
-  '';
 }
