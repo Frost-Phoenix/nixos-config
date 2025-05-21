d476e8d385f9d0c6d6e07a585eb074e6f39a55c7
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-21 16:28:16 +0200

    core: remove zig2nix cache

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -13,14 +13,12 @@
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
         "https://ghostty.cachix.org"
-        "https://zig2nix.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-        "zig2nix.cachix.org-1:LSXEcFKIAN2NtNsznYLMbhEtyHHorV0caI2UgTIxk2k="
       ];
     };
   };
