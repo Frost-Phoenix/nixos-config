9d405fb0f9c17709adf9883e25ee2b8d2d1d1b2c
Frost-Phoenix <67cyril6767@gmail.com>
2025-06-02 11:57:57 +0200

    refactor: sort packages by categories

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -1,64 +1,73 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    ani-cli
+    ## Better core utils
+    eza                               # ls replacement
+    fd                                # find replacement
+    gtrash                            # rm replacement, put deleted files in system trash
+    dysk                              # disk information
+    hevi                              # hex viewer
+    ncdu                              # disk space
+    man-pages                         # extra man pages
+    ripgrep                           # grep replacement
+    tldr
+
+    ## TUI
+    gtt                               # google translate TUI
+    smassh                            # typing test in the terminal
+    toipe                             # typing test in the terminal
+    ttyper                            # cli typing test
+    programmer-calculator
+
     aoc-cli                           # Advent of Code command-line tool
     asciinema
     asciinema-agg
     binsider
     bitwise                           # cli tool for bit / hex manipulation
     caligula                          # User-friendly, lightweight TUI for disk imaging
-    dysk                              # disk information
-    eza                               # ls replacement
+    mimeo
+    swappy                            # snapshot editing tool
+    tdf                               # cli pdf viewer
+    yt-dlp-light
+    woomer
+    xxd
+
+    ## Monitoring
+    htop
+    nitch                             # systhem fetch util
+    onefetch                          # fetch utility for git repo
+    wavemon                           # monitoring for wireless network devices
+
+    ## Fun
+    cbonsai
+    cmatrix
+    pipes
+    sl
+    tty-clock
+
+    ## Multimedia
+    ani-cli
+    imv
+    lowfi
+    mpv
+
+    ## Utilities
     entr                              # perform action when file change
-    fd                                # find replacement
     ffmpeg
     file                              # Show file information
-    gtt                               # google translate TUI
-    gtrash                            # rm replacement, put deleted files in system trash
-    hevi                              # hex viewer
-    htop
-    imv                               # image viewer
     jq                                # JSON processor
     killall
     libnotify
-    lowfi
-    man-pages                         # extra man pages
-    mimeo
-    mpv                               # video player
-    ncdu                              # disk space
-    nitch                             # systhem fetch util
     openssl
-    onefetch                          # fetch utility for git repo
     pamixer                           # pulseaudio command line mixer
     playerctl                         # controller for media players
     poweralertd
-    programmer-calculator
-    ripgrep                           # grep replacement
-    shfmt                             # bash formatter
-    smassh                            # typing test in the terminal
-    swappy                            # snapshot editing tool
-    tdf                               # cli pdf viewer
-    treefmt                           # project formatter
-    tldr
-    toipe                             # typing test in the terminal
-    ttyper                            # cli typing test
     unzip
-    wavemon                           # monitoring for wireless network devices
-    wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
     wget
-    woomer
-    yt-dlp-light
+    wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
     xdg-utils
-    xxd
 
     winetricks
-    wineWowPackages.wayland
-
-    cbonsai                           # terminal screensaver
-    cmatrix
-    pipes                             # terminal screensaver
-    sl
-    tty-clock                         # cli clock
+    wineWowPackages.waylandFull
   ];
 }
