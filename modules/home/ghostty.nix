58cfbfb1c7020294ac69d5030468054638e61dc5
Frost-Phoenix <67cyril6767@gmail.com>
2025-08-03 18:08:48 +0200

    pkgs: ghostty: use home-manager pkg

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -1,88 +1,85 @@
+{ pkgs, host, ... }:
 {
-  inputs,
-  pkgs,
-  host,
-  ...
-}:
-let
-  ghostty = inputs.ghostty.packages.${pkgs.system}.default;
-in
-{
-  home.packages = [ ghostty ];
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
 
-  xdg.configFile."ghostty/config".text = ''
-    # Font
-    font-family = "Maple Mono"
-    font-family = "DejaVu Sans"
-    font-size = ${if (host == "laptop") then "16" else "17"}
-    font-feature = calt
-    font-feature = ss03
+    settings = {
+      font-family = [ "Maple Mono" "DejaVu Sans" ];
+      font-size = "${if (host == "laptop") then "16" else "17"}";
+      font-feature = [ "calt" "ss03" ];
+  
+      bold-is-bright = false;
+      selection-invert-fg-bg = true;
+  
+      # Theme
+      theme = "gruvbox";
+      background-opacity = 0.66;
+  
+      cursor-style = "bar";
+      cursor-style-blink = false;
+      adjust-cursor-thickness = 1;
+  
+      resize-overlay = "never";
+      copy-on-select = false;
+      confirm-close-surface = false;
+      mouse-hide-while-typing = true;
+  
+      window-theme = "ghostty";
+      # window-padding-x = 4;
+      # window-padding-y = 6;
+      window-padding-balance = true;
+      window-padding-color = "background";
+      window-inherit-working-directory = true;
+      window-inherit-font-size = true;
+      window-decoration = false;
+  
+      gtk-titlebar = false;
+      gtk-single-instance = false;
+      gtk-tabs-location = "bottom";
+      gtk-wide-tabs = false;
+  
+      auto-update = "off";
+      term = "ghostty";
+      clipboard-paste-protection = false;
+  
+      keybind = [ 
+        "shift+end=unbind"
+        "shift+home=unbind"
+        "ctrl+shift+left=unbind"
+        "ctrl+shift+right=unbind"
+        "shift+enter=text:\n"
+      ];
+    };
 
-    bold-is-bright = false
-    selection-invert-fg-bg = true
+    themes.gruvbox = {
+      background = "1d2021";
+      foreground = "fbf1c7";
 
-    # Theme
-    theme = "gruvbox"
-    background-opacity = 0.66
+      cursor-color = "D5C4A1";
 
-    cursor-style = bar
-    cursor-style-blink = false
-    adjust-cursor-thickness = 1
+      selection-foreground = "282828";
+      selection-background = "98971A";
 
-    resize-overlay = never
-    copy-on-select = false
-    confirm-close-surface = false
-    mouse-hide-while-typing = true
-
-    window-theme = ghostty
-    # window-padding-x = 4
-    # window-padding-y = 6
-    window-padding-balance = true
-    window-padding-color = background
-    window-inherit-working-directory = true
-    window-inherit-font-size = true
-    window-decoration = false
-
-    gtk-titlebar = false
-    gtk-single-instance = false
-    gtk-tabs-location = bottom
-    gtk-wide-tabs = false
-
-    auto-update = off
-    term = ghostty
-    clipboard-paste-protection = false
-
-    keybind = shift+end=unbind
-    keybind = shift+home=unbind
-    keybind = ctrl+shift+left=unbind
-    keybind = ctrl+shift+right=unbind
-    keybind = shift+enter=text:\n
-  '';
-  xdg.configFile."ghostty/themes/gruvbox".text = ''
-    background = #1d2021
-    foreground = #fbf1c7
-
-    palette = 0=#32302f
-    palette = 1=#cc241d
-    palette = 2=#98971a
-    palette = 3=#d79921
-    palette = 4=#458588
-    palette = 5=#b16286
-    palette = 6=#689d6a
-    palette = 7=#ebdbb2
-
-    palette = 8=#928374
-    palette = 9=#fb4934
-    palette = 10=#b8bb26
-    palette = 11=#fabd2f
-    palette = 12=#83a598
-    palette = 13=#d3869b
-    palette = 14=#8ec07c
-    palette = 15=#fbf1c7
-
-    cursor-color = #D5C4A1
-
-    selection-foreground = #282828
-    selection-background = #98971A
-  '';
+      palette = [
+        "0=#32302f"
+        "1=#cc241d"
+        "2=#98971a"
+        "3=#d79921"
+        "4=#458588"
+        "5=#b16286"
+        "6=#689d6a"
+        "7=#ebdbb2"
+        
+        "8=#928374"
+        "9=#fb4934"
+        "10=#b8bb26"
+        "11=#fabd2f"
+        "12=#83a598"
+        "13=#d3869b"
+        "14=#8ec07c"
+        "15=#fbf1c7"
+      ];
+    };
+  };
 }
