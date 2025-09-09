138c30ded2ec0c5dc50877960f28ac805f2d484c
Frost-Phoenix <67cyril6767@gmail.com>
2025-09-09 14:15:24 +0200

    config: update ssh config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -3,13 +3,24 @@
   programs.ssh = {
     enable = true;
 
-    addKeysToAgent = "1h";
-
-    controlMaster = "auto";
-    controlPath = "~/.ssh/control-%r@%h:%p";
-    controlPersist = "10m";
+    enableDefaultConfig = false;
 
     matchBlocks = {
+      "*" = {
+        addKeysToAgent = "1h";
+
+        controlMaster = "auto";
+        controlPath = "~/.ssh/control-%r@%h:%p";
+        controlPersist = "10m";
+
+        forwardAgent = false;
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+      };
+
       github = {
         host = "github.com";
         hostname = "ssh.github.com";
