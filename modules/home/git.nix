bb9f7cd1e7169eecb2be7093fa015352e63556a8
Patrick Cornelissen <40752251+nikon112@users.noreply.github.com>
2025-09-15 16:46:24 +0200

    Update git.nix

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,8 +3,8 @@
   programs.git = {
     enable = true;
 
-    userName = "Frost-Phoenix";
-    userEmail = "67cyril6767@gmail.com";
+    userName = "nikon112";
+    userEmail = "mail@pjcornelissen.com";
 
     extraConfig = {
       init.defaultBranch = "main";
@@ -17,7 +17,7 @@
           "gh:"
           "https://github.com/"
         ];
-        "git@github.com:frost-phoenix/".insteadOf = "fp:";
+        "git@github.com:nikon112/".insteadOf = "fp:";
       };
       core.excludesFile = "/home/${username}/.config/git/.gitignore";
     };
