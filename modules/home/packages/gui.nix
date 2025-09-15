117bbfa70cd512ecbfc09b29ac8e104caa515ecf
Patrick Cornelissen <40752251+nikon112@users.noreply.github.com>
2025-09-15 15:47:58 +0200

    Add: brave, media-downloader, joplin-desktop, freecad, bambu-studio, proton apps Rem: audacity, obs-studio, ldtk, tiled

--- modules/home/packages/gui.nix
+++ modules/home/packages/gui.nix
@@ -2,26 +2,36 @@
 {
   home.packages = with pkgs; [
     ## Multimedia
-    audacity
+    # audacity
     gimp
-    obs-studio
+    # obs-studio
     pavucontrol
     soundwireserver
     video-trimmer
     vlc
+    brave
 
     ## Office
     libreoffice
     gnome-calculator
+    joplin-desktop
+    freecad
+    bambu-studio
 
     ## Utility
     dconf-editor
     gnome-disk-utility
     mission-center # GUI resources monitor
     zenity
+    media-downloader
+
+    ## Proton apps
+    protonvpn-gui
+    proton-pass
+    proton-authenticator
 
     ## Level editor
-    ldtk
-    tiled
+    # ldtk
+    # tiled
   ];
 }
