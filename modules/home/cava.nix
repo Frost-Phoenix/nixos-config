17c48ffe0e93cbf3321652c9951302e125343e52
Frost-Phoenix <67cyril6767@gmail.com>
2025-09-17 19:13:29 +0200

    config: cava: change gradient colors

--- modules/home/cava.nix
+++ modules/home/cava.nix
@@ -4,23 +4,18 @@
     enable = true;
 
     settings = {
-      general = {
-        autosens = 1;
-        overshoot = 0;
-      };
-
       color = {
         gradient = 1;
         gradient_count = 8;
 
-        gradient_color_1 = "'#99991a'";
-        gradient_color_2 = "'#a28e00'";
-        gradient_color_3 = "'#ab8200'";
-        gradient_color_4 = "'#b37400'";
-        gradient_color_5 = "'#bb6600'";
-        gradient_color_6 = "'#c25400'";
-        gradient_color_7 = "'#c8400d'";
-        gradient_color_8 = "'#cd231d'";
+        gradient_color_1 = "'#fb4934'";   # red
+        gradient_color_2 = "'#fe8019'";   # orange
+        gradient_color_3 = "'#fabd2f'";   # yellow
+        gradient_color_4 = "'#b8bb26'";   # bright green
+        gradient_color_5 = "'#8ec07c'";   # green
+        gradient_color_6 = "'#83a598'";   # blue
+        gradient_color_7 = "'#d3869b'";   # purple
+        gradient_color_8 = "'#ebdbb2'";   # light foreground
       };
     };
   };
