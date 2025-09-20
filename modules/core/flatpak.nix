49ba834b71a1fed9862031380d006e53e443a9ef
Frost-Phoenix <67cyril6767@gmail.com>
2025-09-20 11:09:00 +0200

    pkgs: enable steam and flatpak

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
   services.flatpak = {
-    enable = false;
+    enable = true;
     packages = [
       "com.github.tchx84.Flatseal"
       "io.github.everestapi.Olympus"
