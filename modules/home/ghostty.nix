1f0119689ba18e8e9de6bd46245239081cea4506
Frost-Phoenix <67cyril6767@gmail.com>
2025-09-23 20:23:53 +0200

    config(ghostty): redo the config from scratch

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -1,13 +1,14 @@
-{ pkgs, host, ... }:
+{ host, ... }:
 {
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
 
     settings = {
+      ##### Font #####
       font-family = [
         "Maple Mono"
-        "DejaVu Sans"
+        # "DejaVu Sans"
       ];
       font-size = "${if (host == "laptop") then "16" else "17"}";
       font-feature = [
@@ -15,45 +16,37 @@
         "ss03"
       ];
 
-      bold-is-bright = false;
-      selection-invert-fg-bg = true;
-
-      # Theme
+      ##### Theme #####
       theme = "gruvbox";
-      background-opacity = 0.66;
-
-      cursor-style = "bar";
-      cursor-style-blink = false;
+      background-opacity = 0.5;
       adjust-cursor-thickness = 1;
 
-      resize-overlay = "never";
-      copy-on-select = false;
-      confirm-close-surface = false;
+      selection-clear-on-copy = true;
       mouse-hide-while-typing = true;
 
-      window-theme = "ghostty";
-      # window-padding-x = 4;
-      # window-padding-y = 6;
+      ##### Window #####;
       window-padding-balance = true;
-      window-padding-color = "background";
-      window-inherit-working-directory = true;
-      window-inherit-font-size = true;
-      window-decoration = false;
+      window-padding-color = "extend";
+      window-decoration = "none";
+      window-theme = "ghostty";
+      window-inherit-working-directory = false;
 
-      gtk-titlebar = false;
+      resize-overlay = "never";
+      confirm-close-surface = false;
+      app-notifications = "no-clipboard-copy";
+
       gtk-single-instance = false;
       gtk-tabs-location = "bottom";
       gtk-wide-tabs = false;
 
       auto-update = "off";
-      term = "ghostty";
+
       clipboard-paste-protection = false;
 
+      ##### Keybinds #####
       keybind = [
         "shift+end=unbind"
         "shift+home=unbind"
-        "ctrl+shift+left=unbind"
-        "ctrl+shift+right=unbind"
         "shift+enter=text:\n"
       ];
     };
@@ -64,27 +57,27 @@
 
       cursor-color = "D5C4A1";
 
-      selection-foreground = "282828";
-      selection-background = "98971A";
+      selection-background = "cell-foreground";
+      selection-foreground = "cell-background";
 
       palette = [
-        "0=#32302f"
-        "1=#cc241d"
-        "2=#98971a"
-        "3=#d79921"
-        "4=#458588"
-        "5=#b16286"
-        "6=#689d6a"
-        "7=#ebdbb2"
+        "0=32302f"
+        "1=cc241d"
+        "2=98971a"
+        "3=d79921"
+        "4=458588"
+        "5=b16286"
+        "6=689d6a"
+        "7=ebdbb2"
 
-        "8=#928374"
-        "9=#fb4934"
-        "10=#b8bb26"
-        "11=#fabd2f"
-        "12=#83a598"
-        "13=#d3869b"
-        "14=#8ec07c"
-        "15=#fbf1c7"
+        "8=928374"
+        "9=fb4934"
+        "10=b8bb26"
+        "11=fabd2f"
+        "12=83a598"
+        "13=d3869b"
+        "14=8ec07c"
+        "15=fbf1c7"
       ];
     };
   };
