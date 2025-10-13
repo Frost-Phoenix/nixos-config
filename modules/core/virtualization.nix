4c587c4fcc28e1770eed1c1e5efc949268866b7c
Frost-Phoenix <67cyril6767@gmail.com>
2025-10-13 21:39:24 +0200

    chore: update

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -21,8 +21,6 @@
       enable = true;
       qemu = {
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
     };
     spiceUSBRedirection.enable = true;
