426af6bcd2182576e8b0983596b6f1e1566baf8c
Frost-Phoenix <67cyril6767@gmail.com>
2025-10-14 21:27:21 +0200

    config(zsh): change ls alias

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -22,8 +22,7 @@
       space = "ncdu";
       man = "BAT_THEME='default' batman";
 
-      l = "eza --icons  -a --group-directories-first -1"; # EZA_ICON_SPACING=2
-      ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
+      l = "eza --icons -a --group-directories-first -1 --no-user --long"; # EZA_ICON_SPACING=2
       tree = "eza --icons --tree --group-directories-first";
 
       # Nixos
