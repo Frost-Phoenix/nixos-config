09868237c32963e3dee68b5f099e589b3724d9e9
Frost-Phoenix <67cyril6767@gmail.com>
2025-10-15 12:36:18 +0200

    config(scripts): update nh notification script

--- modules/home/scripts/scripts/nh-error-notify.sh
+++ modules/home/scripts/scripts/nh-error-notify.sh
@@ -1,22 +1,42 @@
 #! /usr/bin/env bash
 
 command="$@"
+terminal_class="com.mitchellh.ghostty"
 
 focus() {
     parent_pid=$PPID
-    terminal_class="com.mitchellh.ghostty"
     hyprctl dispatch focuswindow "class:$terminal_class" > /dev/null
 }
 
-if ! $command; then
-    action=$(
-        notify-send -u critical \
-            --action="focus=focus" \
+start_time=$(date +%s)
+
+$command
+exit_status=$?
+
+end_time=$(date +%s)
+duration=$(($end_time - $start_time))
+
+duration_formatted="$((duration / 60))m $(printf "%02d" $((duration % 60)))s"
+
+active_window_class=$(hyprctl activewindow -j | jq -r '.class')
+
+if [ "$active_window_class" != "$terminal_class" ]; then
+    if [ $exit_status -ne 0 ]; then
+        action=$(
+            notify-send -u critical \
+                --action="focus=focus" \
+                "NixOS Flake" \
+                "\'$command\' failed after $duration_formatted" 2> /dev/null
+        )
+
+        [ "$action" = "focus" ] && focus
+    else
+        notify-send -u normal \
             "NixOS Flake" \
-            "\'$command\' failed at $(date "+%H:%M:%S")" 2> /dev/null
-    )
+            "\'$command\' succeeded after $duration_formatted" 2> /dev/null
+    fi
+fi
 
-    [ "$action" = "focus" ] && focus
-
+if [ $exit_status -ne 0 ]; then
     exit 1
 fi
