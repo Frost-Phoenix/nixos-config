63a927734101c00ca95426c4e416671419005367
Frost-Phoenix <67cyril6767@gmail.com>
2025-10-15 11:37:03 +0200

    pkgs(superfile): disable checkPhase

--- modules/home/superfile/superfile.nix
+++ modules/home/superfile/superfile.nix
@@ -1,6 +1,13 @@
 { pkgs, inputs, ... }:
+let
+  superfile = inputs.superfile.packages.${pkgs.system}.default;
+in
 {
-  home.packages = [ inputs.superfile.packages.${pkgs.system}.default ];
+  home.packages = [
+    (superfile.overrideAttrs (oldAttrs: {
+      doCheck = false;
+    }))
+  ];
 
   xdg.configFile."superfile/config.toml".source = ./config.toml;
 }
