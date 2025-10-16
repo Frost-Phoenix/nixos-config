474b91ceb71eb33e13bb60e5a29bd7280f71bbe8
Frost-Phoenix <67cyril6767@gmail.com>
2025-10-16 16:08:17 +0200

    config(hyprlock): update config

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -6,20 +6,16 @@
     settings = {
       general = {
         hide_cursor = true;
-        no_fade_in = false;
-        disable_loading_bar = true;
         ignore_empty_input = true;
         fractional_scaling = 0;
       };
 
       background = [
         {
-          monitor = "";
           path = "${../../../wallpapers/otherWallpaper/gruvbox/forest_road.jpg}";
+
+          color = "rgba(29, 32, 33, 255)";
           blur_passes = 2;
-          contrast = 0.8916;
-          brightness = 0.8172;
-          vibrancy = 0.1696;
           vibrancy_darkness = 0.0;
         }
       ];
@@ -27,11 +23,13 @@
       shape = [
         # User box
         {
-          monitor = "";
           size = "300, 50";
+
+          rounding = 0;
+          border_size = 2;
           color = "rgba(102, 92, 84, 0.33)";
-          rounding = 10;
-          border_color = "rgba(255, 255, 255, 0)";
+          border_color = "rgba(168, 153, 132, 0.95)";
+
           position = "0, ${if host == "laptop" then "120" else "270"}";
           halign = "center";
           valign = "bottom";
@@ -41,36 +39,42 @@
       label = [
         # Time
         {
-          monitor = "";
           text = ''cmd[update:1000] echo "$(date +'%k:%M')"'';
-          color = "rgba(235, 219, 178, 0.9)";
+
           font_size = 115;
           font_family = "Maple Mono Bold";
+
           shadow_passes = 3;
+          color = "rgba(235, 219, 178, 0.9)";
+
           position = "0, ${if host == "laptop" then "-25" else "-150"}";
           halign = "center";
           valign = "top";
         }
         # Date
         {
-          monitor = "";
           text = ''cmd[update:1000] echo "- $(date +'%A, %B %d') -" '';
-          color = "rgba(235, 219, 178, 0.9)";
+
           font_size = 18;
           font_family = "Maple Mono";
+
           shadow_passes = 3;
+          color = "rgba(235, 219, 178, 0.9)";
+
           position = "0, ${if host == "laptop" then "-225" else "-350"}";
           halign = "center";
           valign = "top";
         }
         # Username
         {
-          monitor = "";
           text = "  $USER";
-          color = "rgba(235, 219, 178, 1)";
+
           font_size = 15;
           font_family = "Maple Mono Bold";
-          position = "0, ${if host == "laptop" then "131" else "281"}";
+
+          color = "rgba(235, 219, 178, 1)";
+
+          position = "0, ${if host == "laptop" then "134" else "281"}";
           halign = "center";
           valign = "bottom";
         }
@@ -78,27 +82,33 @@
 
       input-field = [
         {
-          monitor = "";
           size = "300, 50";
-          outline_thickness = 1;
-          rounding = 10;
-          dots_size = 0.25;
+          rounding = 0;
+          outline_thickness = 2;
+
           dots_spacing = 0.4;
-          dots_center = true;
-          outer_color = "rgba(102, 92, 84, 0.33)";
-          inner_color = "rgba(102, 92, 84, 0.33)";
-          color = "rgba(235, 219, 178, 0.9)";
+
           font_color = "rgba(235, 219, 178, 0.9)";
-          font_size = 14;
           font_family = "Maple Mono Bold";
+
+          outer_color = "rgba(168, 153, 132, 0.95)";
+          inner_color = "rgba(102, 92, 84, 0.33)";
+          check_color = "rgba(152, 151, 26, 0.95)";
+          fail_color = "rgba(204, 36, 29, 0.95)";
+          capslock_color = "rgba(215, 153, 33, 0.95)";
+          bothlock_color = "rgba(215, 153, 33, 0.95)";
+
+          hide_input = false;
           fade_on_empty = false;
           placeholder_text = ''<i><span foreground="##fbf1c7">Enter Password</span></i>'';
-          hide_input = false;
+
           position = "0, ${if host == "laptop" then "50" else "200"}";
           halign = "center";
           valign = "bottom";
         }
       ];
+
+      animation = [ "inputFieldColors, 0" ];
     };
   };
 }
