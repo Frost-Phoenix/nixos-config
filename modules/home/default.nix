1144ba212b4efa3d94227bda08b6d7edcd0dd181
Frost-Phoenix <67cyril6767@gmail.com>
2025-12-22 18:38:15 +0100

    pkgs: remove un-used pkgs

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -20,7 +20,6 @@
     ./lazygit.nix
     ./micro.nix                       # nano replacement
     ./nemo.nix                        # file manager
-    ./nix-search/nix-search.nix       # TUI to search nixpkgs
     ./nvim.nix                        # neovim editor
     ./obsidian.nix
     ./p10k/p10k.nix
--- modules/home/scripts/scripts/lofi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if (ps aux | grep mpv | grep -v grep > /dev/null); then
-    pkill mpv
-else
-    runbg mpv --no-video https://www.youtube.com/live/jfKfPfyJRdk?si=OF0HKrYFFj33BzMo
-fi
