d99c9b8f9fad0a924946c0ea1fd5d6fa463d13c2
Frost-Phoenix <67cyril6767@gmail.com>
2026-01-08 10:50:59 +0100

    fix(pkgs): use older version of gcc env

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,8 @@
   ...
 }:
 {
-  _2048 = pkgs.callPackage ./2048 { };
+  _2048 = pkgs.callPackage ./2048 {
+    stdenv = pkgs.gcc14Stdenv;
+  };
   maple-mono-custom = pkgs.callPackage ./maple-mono { inherit inputs; };
 }
