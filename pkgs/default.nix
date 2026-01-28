08346f2972bb1d358b7eba8e40beece4baa7ab98
Frost-Phoenix <67cyril6767@gmail.com>
2026-01-28 22:30:38 +0100

    chore: fmt

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,9 +5,7 @@
   ...
 }:
 {
-  _2048 = pkgs.callPackage ./2048 {
-    stdenv = pkgs.gcc14Stdenv;
-  };
+  _2048 = pkgs.callPackage ./2048 { stdenv = pkgs.gcc14Stdenv; };
   maple-mono-custom = pkgs.callPackage ./maple-mono { inherit inputs; };
   pomo = pkgs.callPackage ./pomo { };
 }
--- scripts/scripts/ocr.sh
+++ scripts/scripts/ocr.sh
@@ -7,7 +7,7 @@ temp_image=$(mktemp /tmp/ocr-screenshot-XXXXXX.png)
 grimblast --freeze save area "$temp_image"
 
 # Perform OCR and copy to clipboard
-tesseract "$temp_image" stdout 2>/dev/null | wl-copy
+tesseract "$temp_image" stdout 2> /dev/null | wl-copy
 
 # Clean up temporary file
 rm -f "$temp_image"
