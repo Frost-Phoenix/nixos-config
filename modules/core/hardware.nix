27e319e6b45e4654e1e87ed27dbebcc9a3191099
Frost-Phoenix <67cyril6767@gmail.com>
2026-01-28 22:25:11 +0100

    host: add configuration for new p14s host
    This add the p14s host, which is for a thinkpad p14s gen6 amd

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,15 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  hardware = {
-    graphics = {
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-        (intel-vaapi-driver.override { enableHybridCodec = true; })
-        libva-vdpau-driver
-        libvdpau-va-gl
-      ];
-    };
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
+
   hardware.enableRedistributableFirmware = true;
 }
