abf44ff3e5b08e02784fa9900677726af04ac805
Frost-Phoenix <67cyril6767@gmail.com>
2026-01-30 17:52:58 +0100

    config(keybinds): add more audio key support

--- modules/home/swayosd.nix
+++ modules/home/swayosd.nix
@@ -6,25 +6,29 @@
     settings = {
       exec-once = [ "swayosd-server" ];
 
-      bind = [ ",XF86AudioMute, exec, swayosd-client --output-volume mute-toggle" ];
-      # binds active in lockscreen
-      bindl = [
-        ",XF86MonBrightnessUp, exec, swayosd-client --brightness raise 5%+"
-        ",XF86MonBrightnessDown, exec, swayosd-client --brightness lower 5%-"
-        "$mod, XF86MonBrightnessUp, exec, brightnessctl set 100%"
-        "$mod, XF86MonBrightnessDown, exec, brightnessctl set 0%"
+      bind = [
+        ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
+        ", XF86AudioMicMute, exec, toggle-mic"
       ];
-      bindle = [
-        ",XF86AudioRaiseVolume, exec, swayosd-client --output-volume +2"
-        ",XF86AudioLowerVolume, exec, swayosd-client --output-volume -2"
+      bindl = [
+        "$mod, XF86MonBrightnessUp, exec, swayosd-client --brightness 100"
+        "$mod, XF86MonBrightnessDown, exec, swayosd-client --brightness 0"
+      ];
+      bindel = [
+        ", XF86MonBrightnessUp, exec, swayosd-client --brightness raise 5%+"
+        ", XF86MonBrightnessDown, exec, swayosd-client --brightness lower 5%-"
+      ];
+      binde = [
+        ", XF86AudioRaiseVolume, exec, swayosd-client --output-volume +2"
+        ", XF86AudioLowerVolume, exec, swayosd-client --output-volume -2"
 
         "$mod, f11, exec, swayosd-client --output-volume +2"
         "$mod, f12, exec, swayosd-client --output-volume -2"
       ];
       bindr = [
-        "CAPS,Caps_Lock,exec,swayosd-client --caps-lock"
-        ",Scroll_Lock,exec,swayosd-client --scroll-lock"
-        ",Num_Lock,exec,swayosd-client --num-lock"
+        "CAPS, Caps_Lock, exec, swayosd-client --caps-lock"
+        ", Scroll_Lock, exec, swayosd-client --scroll-lock"
+        ", Num_Lock, exec, swayosd-client --num-lock"
       ];
     };
   };
