f864c45c08d90c342f27cbf2b23fb95a8becca80
Frost-Phoenix <67cyril6767@gmail.com>
2026-03-01 11:11:04 +0100

    scripts(monitor-watcher): fix logic for start-up and reloads

--- scripts/scripts/monitor-watcher.sh
+++ scripts/scripts/monitor-watcher.sh
@@ -3,8 +3,6 @@
 # based on https://github.com/ash-17/omarchy-external-monitor/tree/d06a323e7fb39d73be3955b94305830bfdc4a74a
 
 restart-apps() {
-    sleep 1
-
     # restart wallpaper
     pkill .swww-daemon-wr 2> /dev/null
     init-wallpaper &
@@ -18,23 +16,29 @@ restart-apps() {
     swayosd-server &
 }
 
-# listen for hyprland events
+handle-monitor-state() {
+    # is external monitor connected?
+    if hyprctl monitors | grep -qE "HDMI|DP"; then
+        # external monitor connected - disable laptop screen
+        hyprctl keyword monitor "eDP-1,disable"
+    else
+        # no external monitor - enable laptop screen
+        hyprctl keyword monitor "eDP-1,preferred,auto,1"
+    fi
+}
+
+# --- startup check --- #
+sleep 1
+
+handle-monitor-state
+restart-apps
+
+# --- hyprland events listener --- #
 socat -U - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" |
     while read -r line; do
-        # react on external monitor plug/unplug
-        if echo "$line" | grep -qE "monitor(added|removed)>>(HDMI|DP)"; then
-            sleep 1
-
-            # is external monitor connected?
-            if hyprctl monitors | grep -qE "HDMI|DP"; then
-                # external monitor connected - disable laptop screen
-                hyprctl keyword monitor "eDP-1,disable"
-            else
-                # no external monitor - enable laptop screen
-                hyprctl keyword monitor "eDP-1,preferred,auto,1"
-            fi
-
-            # restart some apps after monitor change
+        # react on external monitor plug/unplug or hyprland reload
+        if echo "$line" | grep -qE "monitor(added|removed)>>(HDMI|DP)|configreloaded"; then
+            handle-monitor-state
             restart-apps
         fi
     done
