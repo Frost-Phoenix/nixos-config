13bda073a9004f9ba2368fbc594900ffdd6038be
Frost-Phoenix <67cyril6767@gmail.com>
2026-03-10 08:48:18 +0100

    config(zen-browser): use new config option

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -4,39 +4,6 @@
 
   programs.zen-browser = {
     enable = true;
-    suppressXdgMigrationWarning = true;
+    setAsDefaultBrowser = true;
   };
-
-  xdg.mimeApps =
-    let
-      value =
-        let
-          system = pkgs.stdenv.hostPlatform.system;
-          zen-browser = inputs.zen-browser.packages.${system}.beta;
-        in
-        zen-browser.meta.desktopFileName;
-
-      associations = builtins.listToAttrs (
-        map (name: { inherit name value; }) [
-          "application/x-extension-shtml"
-          "application/x-extension-xhtml"
-          "application/x-extension-html"
-          "application/x-extension-xht"
-          "application/x-extension-htm"
-          "x-scheme-handler/unknown"
-          "x-scheme-handler/mailto"
-          "x-scheme-handler/chrome"
-          "x-scheme-handler/about"
-          "x-scheme-handler/https"
-          "x-scheme-handler/http"
-          "application/xhtml+xml"
-          "application/json"
-          "text/html"
-        ]
-      );
-    in
-    {
-      associations.added = associations;
-      defaultApplications = associations;
-    };
 }
