0f2c684e44ec118b952a529840ace62896560372
Frost-Phoenix <67cyril6767@gmail.com>
2026-03-10 09:12:11 +0100

    fix(tlp): typo

--- hosts/p14s/default.nix
+++ hosts/p14s/default.nix
@@ -27,8 +27,8 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVENOR_ON_AC = "performance";
-        CPU_SCALING_GOVENOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_ON_AC = "performance";
         CPU_ENERGY_PERF_ON_BAT = "power";
