5f7daf887e35c95a5093d015f3d534357a374d15
Frost-Phoenix <67cyril6767@gmail.com>
2026-04-19 20:10:51 +0200

    pkgs(vscodium): remove un-used extentions

--- modules/home/vscodium/extensions.nix
+++ modules/home/vscodium/extensions.nix
@@ -17,11 +17,8 @@ in
       ## Languages
       jnoortheen.nix-ide
       arrterian.nix-env-selector
-      # ms-python.python
       llvm-vs-code-extensions.vscode-clangd
       ziglang.vscode-zig
-      tamasfe.even-better-toml
-      golang.go
 
       ## Color scheme
       jdinhlife.gruvbox
