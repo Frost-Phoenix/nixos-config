cf28476c28390784c04b6f856bcf51482da9f852
Frost-Phoenix <67cyril6767@gmail.com>
2026-05-16 09:47:07 +0200

    pkgs(vscodium): add z80 asm extention

--- modules/home/vscodium/extensions.nix
+++ modules/home/vscodium/extensions.nix
@@ -10,6 +10,16 @@ let
           hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
         };
       };
+  imanolea.z80-asm =
+    pkgs.vscode-utils.buildVscodeMarketplaceExtension
+      {
+        mktplcRef = {
+          name = "z80-asm";
+          publisher = "imanolea";
+          version = "0.0.9";
+          hash = "sha256-uiSEZg9aSMRwdBWAyNtfk9z+3TPflWAv7SKy6qdhvWw=";
+        };
+      };
 in
 {
   programs.vscodium.profiles.default = {
@@ -20,6 +30,7 @@ in
       llvm-vs-code-extensions.vscode-clangd
       ziglang.vscode-zig
       yzhang.markdown-all-in-one
+      imanolea.z80-asm
 
       ## Color scheme
       jdinhlife.gruvbox
